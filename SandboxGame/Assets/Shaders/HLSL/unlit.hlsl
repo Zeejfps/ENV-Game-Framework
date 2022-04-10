446aa81c34f6e474b02f76af2af9cace89938570
Austin Crane <acrane@hitfactor.com>
2022-04-10 15:33:15 -0500

    Update unlit.hlsl

--- SandboxGame/Assets/Shaders/HLSL/unlit.hlsl
+++ SandboxGame/Assets/Shaders/HLSL/unlit.hlsl
@@ -4,7 +4,7 @@
 struct vertexInfo
 {
     float3 position : POSITION;
-    float2 uv: TEXCOORD0;
+    //float2 uv: TEXCOORD0;
     float4 color : COLOR;
 };
 
@@ -15,9 +15,14 @@ struct v2p
     float4 color : TEXCOORD1;
 };
 
+//texture MainTexture;
 // uniforms
-sampler2D MyTexture;
-float2 UVTile;
+//sampler2D MyTexture = sampler_state
+//{
+//    Texture = (ModelTexture));
+//};
+
+//float2 UVTile;
 float4x4 worldViewProjection;
 
 //vert shader func
@@ -25,14 +30,14 @@ v2p vertexShader(vertexInfo input)
 {
     v2p output;
     output.position = mul(worldViewProjection, float4(input.position,1.0));
-    output.uv = input.uv * UVTile;
-    output.color = input.color;
+    //output.uv = input.uv * UVTile;
+    //output.color = input.color;
     return output;
 }
 
 // pixel shader func
 float4 pixelShader(v2p input) : SV_TARGET
 {
-    float4 color = tex2D(MyTexture, input.uv);
-    return color * input.color;
+    //float4 color = tex2D(MyTexture, input.uv);
+    return float4(1,0,0,1);
 }
