3825e1256d6fc7e48b479c29e4999ffffb7bd65c
Zee Vasilyev <Zeejfps@gmail.com>
2022-04-11 13:54:55 -0500

    Working on tweaks to the Material_GL

--- Framework/IMaterial.cs
+++ Framework/IMaterial.cs
@@ -7,11 +7,12 @@ public interface IMaterial : IAsset
     string Shader { get; }
     void Apply(IShaderProgram shaderProgram);
 
+    void SetFloat(string propertyName, float value);
+
     void SetVector3(string propertyName, float x, float y, float z);
-    void SetVector3(string propertyName, Vector3 x);
+    void SetVector3(string propertyName, Vector3 vector);
 
     void SetTexture2d(string propertyName, ITexture texture);
+    
     void SetMatrix4x4(string propertyName, Matrix4x4 matrix);
-
-    void SetFloat(string propertyName, float x);
 }
--- GlfwOpenGLBackend/Material_GL.cs
+++ GlfwOpenGLBackend/Material_GL.cs
@@ -1,10 +1,13 @@
 using System.Numerics;
 using Framework;
+using static OpenGL.Gl;
 
 namespace GlfwOpenGLBackend.AssetLoaders;
 
 public class Material_GL : IMaterial
 {
+    private readonly Dictionary<string, int> m_PropertyToIdMap = new();
+
     public string Shader { get; }
     public bool IsLoaded { get; private set; }
 
@@ -18,7 +21,7 @@ public class Material_GL : IMaterial
 
     public void Use()
     {
-        
+        glUseProgram(m_ProgramId);
     }
     
     public void Unload()
@@ -30,15 +33,22 @@ public class Material_GL : IMaterial
     {
         throw new NotImplementedException();
     }
+    
+    public void SetFloat(string propertyName, float value)
+    {
+        var location = GetUniformLocation(propertyName);
+        glUniform1f(location, value);
+    }
 
     public void SetVector3(string propertyName, float x, float y, float z)
     {
-        throw new NotImplementedException();
+        var location = GetUniformLocation(propertyName);
+        glUniform3f(location, x, y, z);
     }
 
-    public void SetVector3(string propertyName, Vector3 x)
+    public void SetVector3(string propertyName, Vector3 vector)
     {
-        throw new NotImplementedException();
+        SetVector3(propertyName, vector.X, vector.Y, vector.Z);
     }
 
     public void SetTexture2d(string propertyName, ITexture texture)
@@ -48,11 +58,22 @@ public class Material_GL : IMaterial
 
     public void SetMatrix4x4(string propertyName, Matrix4x4 matrix)
     {
-        throw new NotImplementedException();
+        var location = GetUniformLocation(propertyName);
+        unsafe
+        {
+            var p = &matrix.M11;
+            glUniformMatrix4fv(location, 1, false, p);
+        }
     }
-
-    public void SetFloat(string propertyName, float x)
+    
+    private int GetUniformLocation(string uniformName)
     {
-        throw new NotImplementedException();
+        if (!m_PropertyToIdMap.TryGetValue(uniformName, out var location))
+        {
+            location = glGetUniformLocation(m_ProgramId, uniformName);
+            m_PropertyToIdMap[uniformName] = location;
+        }
+
+        return location;
     }
 }
