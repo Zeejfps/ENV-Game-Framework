9b949eba0295c03a5269db3cce06ad7fa22e3d75
Zee Vasilyev <zvasilyev@hitfactor.com>
2022-04-13 22:34:36 -0500

    Gave asset database some love

--- GlfwOpenGLBackend/AssetLoaders/MeshAssetLoader_GL.cs
+++ GlfwOpenGLBackend/AssetLoaders/MeshAssetLoader_GL.cs
@@ -5,19 +5,13 @@ namespace Framework;
 
 public class MeshAssetLoader_GL : IAssetLoader<IMesh>
 {
-    private readonly Dictionary<string, IMesh> m_PathToAssetMap = new Dictionary<string, IMesh>();
-
     public IAsset LoadAsset(string assetPath)
     {
-        if (m_PathToAssetMap.TryGetValue(assetPath, out var mesh))
-            return mesh;
-        
         using var stream = File.Open(assetPath, FileMode.Open);
         using var reader = new BinaryReader(stream);
 
         using var meshAsset = MeshAsset_GL.Deserialize(reader);
-        mesh = new Mesh_GL(meshAsset.Vertices, meshAsset.Normals, meshAsset.Uvs, meshAsset.Tangents, meshAsset.Triangles);
-        m_PathToAssetMap[assetPath] = mesh;
+        var mesh = new Mesh_GL(meshAsset.Vertices, meshAsset.Normals, meshAsset.Uvs, meshAsset.Tangents, meshAsset.Triangles);
         return mesh;
     }
 }
