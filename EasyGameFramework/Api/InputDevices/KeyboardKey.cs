2993edd2e498a93026bcc03ea5eac95658748f0e
Zee Vasilyev <zeejfps@gmail.com>
2022-08-31 09:33:50 -0500

    Pulled the MouseButton and KeyboardKey classes into their own files

---
+++ EasyGameFramework/Api/InputDevices/KeyboardKey.cs
@@ -0,0 +1,40 @@
+﻿namespace EasyGameFramework.Api.InputDevices;
+
+public enum KeyboardKey
+{
+    Unknown,
+
+    Alpha0,
+    Alpha1,
+    Alpha2,
+    Alpha3,
+    Alpha4,
+    
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    Q,
+    R,
+    S,
+    T,
+    W,
+    Y,
+
+    Space,
+    Escape,
+    Apostrophe,
+
+    Comma,
+    Period,
+    Slash,
+
+    Minus,
+}
---
+++ EasyGameFramework/Api/InputDevices/MouseButton.cs
@@ -0,0 +1,40 @@
+﻿namespace EasyGameFramework.Api.InputDevices;
+
+public readonly struct MouseButton : IEquatable<MouseButton>
+{
+    public static readonly MouseButton Left = new(0);
+    public static readonly MouseButton Right = new(1);
+    public static readonly MouseButton Middle = new(3);
+
+    private int ButtonId { get; }
+
+    public MouseButton(int id)
+    {
+        ButtonId = id;
+    }
+
+    public bool Equals(MouseButton other)
+    {
+        return ButtonId == other.ButtonId;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is MouseButton other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return ButtonId;
+    }
+
+    public static bool operator ==(MouseButton left, MouseButton right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(MouseButton left, MouseButton right)
+    {
+        return !left.Equals(right);
+    }
+}
