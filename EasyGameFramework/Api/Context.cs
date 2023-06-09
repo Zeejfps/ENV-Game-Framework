bfe3c44a9faa883f0d0753dc4043e86324d13413
Zee Vasilyev <zeejfps@gmail.com>
2023-06-09 09:19:43 -0500

    Slowly gutting the IContext idea

--- EasyGameFramework/Api/Context.cs
+++ EasyGameFramework/Api/Context.cs
@@ -6,20 +6,14 @@ public sealed class Context : IContext
 {
     public Context(
         IWindow window,
-        IInputSystem input, 
-        ILogger logger,
-        IContainer container)
+        IInputSystem input)
     {
         Window = window;
         Input = input;
         Gpu = window.Gpu;
-        Logger = logger;
-        Container = container;
     }
     
     public IWindow Window { get; }
     public IInputSystem Input { get; }
     public IGpu Gpu { get; }
-    public ILogger Logger { get; }
-    public IContainer Container { get; }
 }
--- EasyGameFramework/Api/IContext.cs
+++ EasyGameFramework/Api/IContext.cs
@@ -7,6 +7,4 @@ public interface IContext
     IWindow Window { get; }
     IInputSystem Input { get; }
     IGpu Gpu { get; }
-    ILogger Logger { get; }
-    IContainer Container { get; }
 }
