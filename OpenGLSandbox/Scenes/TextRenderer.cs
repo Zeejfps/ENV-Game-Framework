66247438ca926007e496b69e1a6ed525eb183204
Zee Vasilyev <zeejfps@gmail.com>
2023-09-26 16:13:33 -0500

    starting to think through how I would handle text styling and the likes

--- OpenGLSandbox/Scenes/TextRenderer.cs
+++ OpenGLSandbox/Scenes/TextRenderer.cs
@@ -1,8 +1,22 @@
 ﻿using System.Numerics;
 using System.Text;
 using BmFont;
+using static GL46;
+using static OpenGLSandbox.Utils_GL;
 
 namespace OpenGLSandbox;
+
+public enum TextAlignment
+{
+    Start, Center, End, Justify
+}
+
+public struct TextStyle
+{
+    public TextAlignment HorizontalTextAlignment;
+    public TextAlignment VerticalTextAlignment;
+    public bool Wrap;
+}
 
 public sealed unsafe class TextRenderer : IDisposable
 {
@@ -29,84 +43,84 @@ public sealed unsafe class TextRenderer : IDisposable
     public TextRenderer()
     {
         uint vao;
-        GL46.glGenVertexArrays(1, &vao);
-        Utils_GL.AssertNoGlError();
+        glGenVertexArrays(1, &vao);
+        AssertNoGlError();
         m_Vao = vao;
 
         uint vbo;
-        GL46.glGenBuffers(1, &vbo);
-        Utils_GL.AssertNoGlError();
+        glGenBuffers(1, &vbo);
+        AssertNoGlError();
         m_Vbo = vbo;
         
-        GL46.glBindVertexArray(vao);
-        Utils_GL.AssertNoGlError();
+        glBindVertexArray(vao);
+        AssertNoGlError();
         
-        GL46.glBindBuffer(GL46.GL_ARRAY_BUFFER, vbo);
-        Utils_GL.AssertNoGlError();
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        AssertNoGlError();
 
         var texturedQuad = new TexturedQuad();
-        GL46.glBufferData(GL46.GL_ARRAY_BUFFER, new IntPtr(sizeof(TexturedQuad)), &texturedQuad, GL46.GL_STATIC_DRAW);
-        Utils_GL.AssertNoGlError();
+        glBufferData(GL_ARRAY_BUFFER, new IntPtr(sizeof(TexturedQuad)), &texturedQuad, GL_STATIC_DRAW);
+        AssertNoGlError();
 
         uint positionAttribLocation = 0;
-        GL46.glVertexAttribPointer(positionAttribLocation, 2, GL46.GL_FLOAT, false, sizeof(TexturedQuad.Vertex), Utils_GL.Offset(0));
-        Utils_GL.AssertNoGlError();
-        GL46.glEnableVertexAttribArray(positionAttribLocation);
-        Utils_GL.AssertNoGlError();
+        glVertexAttribPointer(positionAttribLocation, 2, GL_FLOAT, false, sizeof(TexturedQuad.Vertex), Offset(0));
+        AssertNoGlError();
+        glEnableVertexAttribArray(positionAttribLocation);
+        AssertNoGlError();
 
         uint texCoordsAttribLocation = 1;
-        GL46.glVertexAttribPointer(texCoordsAttribLocation, 2, GL46.GL_FLOAT, false, sizeof(TexturedQuad.Vertex), Utils_GL.Offset<TexturedQuad.Vertex>(nameof(TexturedQuad.Vertex.TexCoords)));
-        Utils_GL.AssertNoGlError();
-        GL46.glEnableVertexAttribArray(texCoordsAttribLocation);
-        Utils_GL.AssertNoGlError();
+        glVertexAttribPointer(texCoordsAttribLocation, 2, GL_FLOAT, false, sizeof(TexturedQuad.Vertex), Offset<TexturedQuad.Vertex>(nameof(TexturedQuad.Vertex.TexCoords)));
+        AssertNoGlError();
+        glEnableVertexAttribArray(texCoordsAttribLocation);
+        AssertNoGlError();
 
         uint perInstanceBuffer;
-        GL46.glGenBuffers(1, &perInstanceBuffer);
-        Utils_GL.AssertNoGlError();
+        glGenBuffers(1, &perInstanceBuffer);
+        AssertNoGlError();
         m_PerInstanceBuffer = perInstanceBuffer;
 
-        GL46.glBindBuffer(GL46.GL_ARRAY_BUFFER, perInstanceBuffer);
-        Utils_GL.AssertNoGlError();
+        glBindBuffer(GL_ARRAY_BUFFER, perInstanceBuffer);
+        AssertNoGlError();
         
         var maxCharCount = 256;
-        GL46.glBufferData(GL46.GL_ARRAY_BUFFER, new IntPtr(maxCharCount * sizeof(PerInstanceData)), (void*)0, GL46.GL_STREAM_DRAW);
-        Utils_GL.AssertNoGlError();
+        glBufferData(GL_ARRAY_BUFFER, new IntPtr(maxCharCount * sizeof(PerInstanceData)), (void*)0, GL_STREAM_DRAW);
+        AssertNoGlError();
 
         uint positionRectAttribLocation = 2;
-        GL46.glVertexAttribPointer(positionRectAttribLocation, 4, GL46.GL_FLOAT, false, sizeof(PerInstanceData), Utils_GL.Offset<PerInstanceData>(nameof(PerInstanceData.PositionRect)));
-        GL46.glEnableVertexAttribArray(positionRectAttribLocation);
-        GL46.glVertexAttribDivisor(positionRectAttribLocation, 1);
-        Utils_GL.AssertNoGlError();
+        glVertexAttribPointer(positionRectAttribLocation, 4, GL_FLOAT, false, sizeof(PerInstanceData), Offset<PerInstanceData>(nameof(PerInstanceData.PositionRect)));
+        glEnableVertexAttribArray(positionRectAttribLocation);
+        glVertexAttribDivisor(positionRectAttribLocation, 1);
+        AssertNoGlError();
         
         // Location in the glyph sheet
         uint glyphSheetRectAttribLocation = 3;
-        GL46.glVertexAttribPointer(glyphSheetRectAttribLocation, 4, GL46.GL_FLOAT, false, sizeof(PerInstanceData), Utils_GL.Offset<PerInstanceData>(nameof(PerInstanceData.GlyphSheetRect)));
-        GL46.glEnableVertexAttribArray(glyphSheetRectAttribLocation);
-        GL46.glVertexAttribDivisor(glyphSheetRectAttribLocation, 1);
-        Utils_GL.AssertNoGlError();
+        glVertexAttribPointer(glyphSheetRectAttribLocation, 4, GL_FLOAT, false, sizeof(PerInstanceData), Offset<PerInstanceData>(nameof(PerInstanceData.GlyphSheetRect)));
+        glEnableVertexAttribArray(glyphSheetRectAttribLocation);
+        glVertexAttribDivisor(glyphSheetRectAttribLocation, 1);
+        AssertNoGlError();
         
         // NOTE(Zee): I am going to make color a per instance variable on purpose
         // This allows us to color each letter differently instead of the whole text
         uint colorRectAttribLocation = 4;
-        GL46.glVertexAttribPointer(colorRectAttribLocation, 4, GL46.GL_FLOAT, false, sizeof(PerInstanceData), Utils_GL.Offset<PerInstanceData>(nameof(PerInstanceData.Color)));
-        GL46.glEnableVertexAttribArray(colorRectAttribLocation);
-        GL46.glVertexAttribDivisor(colorRectAttribLocation, 1);
-        Utils_GL.AssertNoGlError();
+        glVertexAttribPointer(colorRectAttribLocation, 4, GL_FLOAT, false, sizeof(PerInstanceData), Offset<PerInstanceData>(nameof(PerInstanceData.Color)));
+        glEnableVertexAttribArray(colorRectAttribLocation);
+        glVertexAttribDivisor(colorRectAttribLocation, 1);
+        AssertNoGlError();
         
         m_ShaderProgram = new ShaderProgramBuilder()
             .WithVertexShader("Assets/Shaders/bmpfont.vert.glsl")
             .WithFragmentShader("Assets/Shaders/bmpfont.frag.glsl")
             .Build();
 
-        GL46.glUseProgram(m_ShaderProgram);
-        Utils_GL.AssertNoGlError();
+        glUseProgram(m_ShaderProgram);
+        AssertNoGlError();
 
         var projectionMatrixUniformLocation = GetUniformLocation("u_ProjectionMatrix");
         Console.WriteLine("Projection Matrix Uniform Location: " + projectionMatrixUniformLocation);
         
         var projectionMatrix = Matrix4x4.CreateOrthographicOffCenter(0f, 640f, 0f, 640f, 0.1f, 100f);
-        GL46.glUniformMatrix4fv(projectionMatrixUniformLocation, 1, false, &projectionMatrix.M11);
-        Utils_GL.AssertNoGlError();
+        glUniformMatrix4fv(projectionMatrixUniformLocation, 1, false, &projectionMatrix.M11);
+        AssertNoGlError();
         
         var font = FontLoader.Load("Assets/bitmapfonts/test.fnt");
         foreach (var glyph in font.Chars)
@@ -118,21 +132,21 @@ public sealed unsafe class TextRenderer : IDisposable
         m_Base = font.Common.Base;
 
         uint tex;
-        GL46.glGenTextures(1, &tex);
-        Utils_GL.AssertNoGlError();
+        glGenTextures(1, &tex);
+        AssertNoGlError();
         m_Tex = tex;
         
-        GL46.glActiveTexture(GL46.GL_TEXTURE0);
-        Utils_GL.AssertNoGlError();
-        GL46.glBindTexture(GL46.GL_TEXTURE_2D, tex);
-        Utils_GL.AssertNoGlError();
+        glActiveTexture(GL_TEXTURE0);
+        AssertNoGlError();
+        glBindTexture(GL_TEXTURE_2D, tex);
+        AssertNoGlError();
         
-        GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_MAX_LEVEL, 0);
-        GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_WRAP_S, GL46.GL_REPEAT);
-        GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_WRAP_T, GL46.GL_REPEAT);
-        GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_MIN_FILTER, GL46.GL_NEAREST);
-        GL46.glTexParameteri(GL46.GL_TEXTURE_2D, GL46.GL_TEXTURE_MAG_FILTER, GL46.GL_NEAREST);
-        Utils_GL.AssertNoGlError();
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        AssertNoGlError();
 
         var image = new TgaImage("Assets/bitmapfonts/test_0.tga");
         image.UploadToGpu();
@@ -140,11 +154,16 @@ public sealed unsafe class TextRenderer : IDisposable
 
     public int LineHeight => m_LineHeight;
 
+    public void RenderText(Rect screenRect, TextStyle style, ReadOnlySpan<char> text)
+    {
+        
+    }
+    
     public void RenderText(int x, int y, Color color, ReadOnlySpan<char> text)
     {
         var cursor = new Vector2(x, y);
-        GL46.glBindBuffer(GL46.GL_ARRAY_BUFFER, m_PerInstanceBuffer);
-        var bufferPtr = GL46.glMapBuffer(GL46.GL_ARRAY_BUFFER, GL46.GL_WRITE_ONLY);
+        glBindBuffer(GL_ARRAY_BUFFER, m_PerInstanceBuffer);
+        var bufferPtr = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
         var buffer = new Span<PerInstanceData>(bufferPtr, text.Length);
         var i = 0;
         foreach (var c in text)
@@ -178,9 +197,9 @@ public sealed unsafe class TextRenderer : IDisposable
             cursor.X += glyph.XAdvance;
         }
 
-        GL46.glUnmapBuffer(GL46.GL_ARRAY_BUFFER);
+        glUnmapBuffer(GL_ARRAY_BUFFER);
         
-        GL46.glDrawArraysInstanced(GL46.GL_TRIANGLES, 0, 6, text.Length);
+        glDrawArraysInstanced(GL_TRIANGLES, 0, 6, text.Length);
     }
 
     private int GetUniformLocation(string uniformName)
@@ -188,26 +207,26 @@ public sealed unsafe class TextRenderer : IDisposable
         var uniformNameAsAsciiBytes = Encoding.ASCII.GetBytes(uniformName);
         int uniformLocation;
         fixed(byte* ptr = &uniformNameAsAsciiBytes[0])
-            uniformLocation = GL46.glGetUniformLocation(m_ShaderProgram, ptr);
-        Utils_GL.AssertNoGlError();
+            uniformLocation = glGetUniformLocation(m_ShaderProgram, ptr);
+        AssertNoGlError();
         return uniformLocation;
     }
     
     public void Dispose()
     {
         fixed (uint* vao = &m_Vao)
-            GL46.glDeleteVertexArrays(1, vao);
+            glDeleteVertexArrays(1, vao);
         
         fixed (uint* vbo = &m_Vbo)
-            GL46.glDeleteBuffers(1, vbo);
+            glDeleteBuffers(1, vbo);
         
         fixed (uint* buffer = &m_PerInstanceBuffer)
-            GL46.glDeleteBuffers(1, buffer);
+            glDeleteBuffers(1, buffer);
         
         fixed (uint* tex = &m_Tex)
-            GL46.glDeleteTextures(1, tex);
+            glDeleteTextures(1, tex);
         
-        GL46.glDeleteProgram(m_ShaderProgram);
+        glDeleteProgram(m_ShaderProgram);
     }
 
     private FontChar GetGlyph(char c)
