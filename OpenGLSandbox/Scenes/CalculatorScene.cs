800f77c63a22ab19715e810caf27e15ae61e07f2
Zee Vasilyev <zeejfps@gmail.com>
2023-09-30 18:40:15 -0500

    Cleaned up the file a little bit

--- OpenGLSandbox/Scenes/CalculatorScene.cs
+++ OpenGLSandbox/Scenes/CalculatorScene.cs
@@ -15,44 +15,10 @@ public sealed class CalculatorScene : IScene
     
     private readonly PanelRenderer m_PanelRenderer;
     private readonly BitmapFontTextRenderer m_TextRenderer;
-    
-    private TextButtonOld? m_TextButton;
 
-    private readonly List<WidgetOld> m_Widgets = new();
+    private IBuildContext m_Context;
+    private IWidget m_CalculatorWidget;
 
-    private readonly PanelStyle m_ButtonStyleNormal = new()
-    {
-        BackgroundColor = Color.FromHex(0x3e3940, 1f),
-        BorderRadius = new Vector4(7f, 7f, 7f, 7f),
-        BorderColor = Color.FromHex(0x322e35, 1f),
-        BorderSize = BorderSize.All(1f)
-    };
-    
-    private readonly PanelStyle m_ButtonStylePressed = new()
-    {
-        BackgroundColor = Color.FromHex(0x2a262e, 1f),
-        BorderRadius = new Vector4(7f, 7f, 7f, 7f),
-        BorderColor = Color.FromHex(0x332e35, 1f),
-        BorderSize = BorderSize.All(1f)
-    };
-
-    private readonly TextStyle m_ButtonTextStyleNormal = new()
-    {
-        Color = Color.FromHex(0xf7f7f7, 1f),
-        VerticalTextAlignment = TextAlignment.Center,
-        HorizontalTextAlignment = TextAlignment.Center
-    };
-    
-    private readonly TextStyle m_ButtonTextStylePressed = new()
-    {
-        Color = Color.FromHex(0xc8c7c9, 1f),
-        VerticalTextAlignment = TextAlignment.Center,
-        HorizontalTextAlignment = TextAlignment.Center
-    };
-
-    private TextButtonWidget test;
-    private IBuildContext context;
-    
     public CalculatorScene(IWindow window, IInputSystem inputSystem)
     {
         m_Window = window;
@@ -60,8 +26,8 @@ public sealed class CalculatorScene : IScene
         m_PanelRenderer = new PanelRenderer(window);
         m_TextRenderer = new BitmapFontTextRenderer(window, "Assets/bitmapfonts/Segoe UI.fnt");
 
-        context = new TestContext(m_PanelRenderer, m_TextRenderer, inputSystem, window);
-        test = new TextButtonWidget
+        m_Context = new TestContext(m_PanelRenderer, m_TextRenderer, inputSystem, window);
+        m_CalculatorWidget = new TextButtonWidget
         {
             ScreenRect = new Rect(50, 50, 200, 50)
         };
@@ -106,36 +72,15 @@ public sealed class CalculatorScene : IScene
         m_PanelRenderer.Load();
         m_TextRenderer.Load();
         
-        // m_TextButton = new TextButtonOld(m_PanelRenderer, m_TextRenderer)
-        // {
-        //     ScreenRect = new Rect(10f, 10f, 200f, 60f),
-        //     Text = "+/-",
-        //     PanelStyleNormal = m_ButtonStyleNormal,
-        //     PanelStylePressed = m_ButtonStylePressed,
-        //     TextStyleNormal = m_ButtonTextStyleNormal,
-        //     TextStylePressed = m_ButtonTextStylePressed,
-        //     IsVisible = true
-        // };
-        //m_Widgets.Add(m_TextButton);
-
         var bgColor = Color.FromHex(0x221e26, 1f);
         glClearColor(bgColor.R, bgColor.G, bgColor.B, bgColor.A);
     }
 
     public void Update()
     {
-        // var mouse = m_InputSystem.Mouse;
-        // var mouseScreenPosition = new Vector2(mouse.ScreenX, m_Window.ScreenHeight - mouse.ScreenY);
-        // foreach (var widget in m_Widgets)
-        // {
-        //     widget.IsHovered = widget.ScreenRect.Contains(mouseScreenPosition);
-        //     widget.IsPressed = widget.IsHovered && mouse.IsButtonPressed(MouseButton.Left);
-        //     widget.Update();
-        // }
-        
         glClear(GL_COLOR_BUFFER_BIT);
         
-        test.Update(context);
+        m_CalculatorWidget.Update(m_Context);
         
         m_PanelRenderer.Update();
         m_TextRenderer.Update();
@@ -143,7 +88,6 @@ public sealed class CalculatorScene : IScene
 
     public void Unload()
     {
-        m_Widgets.Clear();
         m_PanelRenderer.Unload();
         m_TextRenderer.Unload();
     }
@@ -180,7 +124,7 @@ public sealed class CalculatorScene : IScene
     {
         private bool m_IsDirty;
         
-        public sealed override void Update(IBuildContext context)
+        public override void Update(IBuildContext context)
         {
             if (m_IsDirty)
             {
@@ -190,9 +134,14 @@ public sealed class CalculatorScene : IScene
             base.Update(context);
         }
 
-        protected void SetDirty()
+        protected bool SetField<T>(ref T field, T value)
         {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
             m_IsDirty = true;
+            return true;
         }
     }
     
@@ -210,7 +159,7 @@ public sealed class CalculatorScene : IScene
         
         protected override IWidget Build(IBuildContext context)
         {
-            Console.WriteLine("Build:TextWidget");
+            //Console.WriteLine("Build:TextWidget");
             var renderer = context.Get<ITextRenderer>();
             m_RenderedText = renderer.Render(Text, ScreenRect, Style);
             return this;
@@ -224,7 +173,7 @@ public sealed class CalculatorScene : IScene
 
     sealed class MultiChildWidget : IWidget
     {
-        private IEnumerable<IWidget> m_Children;
+        private readonly IEnumerable<IWidget> m_Children;
 
         public Rect ScreenRect { get; set; }
 
@@ -249,15 +198,12 @@ public sealed class CalculatorScene : IScene
     sealed class StackWidget : Widget
     {
         public List<IWidget> Children { get; init; } = new();
-        
 
         protected override IWidget Build(IBuildContext context)
         {
-            Console.WriteLine("Build:StackWidget");
+            //Console.WriteLine("Build:StackWidget");
             foreach (var widget in Children)
-            {
                 widget.ScreenRect = ScreenRect;
-            }
             return new MultiChildWidget(Children);
         }
     }
@@ -270,7 +216,7 @@ public sealed class CalculatorScene : IScene
         
         protected override IWidget Build(IBuildContext context)
         {
-            Console.WriteLine("Build:PanelWidget");
+            //Console.WriteLine("Build:PanelWidget");
             var renderer = context.Get<IPanelRenderer>();
             m_RenderedPanel = renderer.Render(ScreenRect, Style);
             return this;
@@ -283,27 +229,44 @@ public sealed class CalculatorScene : IScene
         }
     }
 
-    sealed class InputHandlerWidget : Widget
+    sealed class InputListenerWidget : Widget
     {
         public Action? OnPointerEnter { get; set; }
         public Action? OnPointerExit { get; set; }
+        public Action? OnPointerPressed { get; set; }
+        public Action? OnPointerReleased { get; set; }
         
         public IWidget Child { get; init; }
 
-        private bool m_IsPointerHovering;
-
+        private bool m_IsHovered;
         private bool IsPointerHovering
         {
-            get => m_IsPointerHovering;
+            get => m_IsHovered;
             set
             {
-                if (m_IsPointerHovering == value)
+                if (m_IsHovered == value)
                     return;
-                m_IsPointerHovering = value;
-                if (m_IsPointerHovering)
+                m_IsHovered = value;
+                if (m_IsHovered)
                     OnPointerEnter?.Invoke();
                 else
                     OnPointerExit?.Invoke();
+            }
+        }
+
+        private bool m_IsPressed;
+        public bool IsPressed
+        {
+            get => m_IsPressed;
+            set
+            {
+                if (m_IsPressed == value)
+                    return;
+                m_IsPressed = value;
+                if(m_IsPressed)
+                    OnPointerPressed?.Invoke();
+                else
+                    OnPointerReleased?.Invoke();
             }
         }
 
@@ -312,7 +275,7 @@ public sealed class CalculatorScene : IScene
 
         protected override IWidget Build(IBuildContext context)
         {
-            Console.WriteLine("Build:InputHandlerWidget");
+            //Console.WriteLine("Build:InputHandlerWidget");
             
             var window = context.Get<IWindow>();
             ScreenHeight = window.ScreenHeight;
@@ -320,10 +283,12 @@ public sealed class CalculatorScene : IScene
             var inputSystem = context.Get<IInputSystem>();
             m_Mouse = inputSystem.Mouse;
             m_Mouse.Moved += Mouse_OnMoved;
+            m_Mouse.ButtonStateChanged += Mouse_OnButtonStateChanged;
             
             Child.ScreenRect = ScreenRect;
 
-            m_IsPointerHovering = ScreenRect.Contains(m_Mouse.ScreenX, ScreenHeight - m_Mouse.ScreenY);
+            m_IsHovered = ScreenRect.Contains(m_Mouse.ScreenX, ScreenHeight - m_Mouse.ScreenY);
+            m_IsPressed = m_IsHovered && m_Mouse.IsButtonPressed(MouseButton.Left);
             
             return Child;
         }
@@ -331,7 +296,10 @@ public sealed class CalculatorScene : IScene
         public override void Dispose()
         {
             if (m_Mouse != null)
+            {
                 m_Mouse.Moved -= Mouse_OnMoved;
+                m_Mouse.ButtonStateChanged -= Mouse_OnButtonStateChanged;
+            }
             base.Dispose();
         }
 
@@ -339,6 +307,13 @@ public sealed class CalculatorScene : IScene
         {
             var mouse = evt.Mouse;
             IsPointerHovering = ScreenRect.Contains(mouse.ScreenX, ScreenHeight - mouse.ScreenY);
+            if (IsPressed && !IsPointerHovering) IsPressed = false;
+        }
+
+        private void Mouse_OnButtonStateChanged(in MouseButtonStateChangedEvent evt)
+        {
+            var mouse = evt.Mouse;
+            IsPressed = m_IsHovered && mouse.IsButtonPressed(MouseButton.Left);
         }
     }
 
@@ -349,232 +324,96 @@ public sealed class CalculatorScene : IScene
     
     sealed class TextButtonWidget : StatefulWidget
     {
-        private bool m_IsHovered;
-
-        private bool IsHovered
+        private readonly PanelStyle m_PanelStyleNormal = new()
         {
-            get => m_IsHovered;
-            set
-            {
-                if (m_IsHovered == value)
-                    return;
-                m_IsHovered = value;
-                SetDirty();
-            }
-        }
+            BackgroundColor = Color.FromHex(0x3e3940, 1f),
+            BorderRadius = new Vector4(7f, 7f, 7f, 7f),
+            BorderColor = Color.FromHex(0x322e35, 1f),
+            BorderSize = BorderSize.All(1f)
+        };
         
-        protected override IWidget Build(IBuildContext context)
+        private readonly PanelStyle m_PanelStyleHovered = new()
         {
-            Console.WriteLine("Build:TextButtonWidget");
+            BackgroundColor = Color.FromHex(0x382e3c, 1f),
+            BorderRadius = new Vector4(7f, 7f, 7f, 7f),
+            BorderColor = Color.FromHex(0x332e35, 1f),
+            BorderSize = BorderSize.All(1f)
+        };
+    
+        private readonly PanelStyle m_PanelStylePressed = new()
+        {
+            BackgroundColor = Color.FromHex(0x2a262e, 1f),
+            BorderRadius = new Vector4(7f, 7f, 7f, 7f),
+            BorderColor = Color.FromHex(0x332e35, 1f),
+            BorderSize = BorderSize.All(1f)
+        };
             
-            var panelBackgroundColor = Color.FromHex(0xff00ff, 0f);
-            if (IsHovered)
-            {
-                panelBackgroundColor = Color.FromHex(0x11ff22, 1f);
-            }
-
-            return new InputHandlerWidget
-            {
-                OnPointerEnter = () =>
-                {
-                    IsHovered = true;
-                },
-                
-                OnPointerExit = () =>
-                {
-                    IsHovered = false;
-                },
-
-                ScreenRect = ScreenRect,
-                Child = new StackWidget
-                {
-                    Children = new List<IWidget>
-                    {
-                        new PanelWidget
-                        {
-                            Style = new PanelStyle
-                            {
-                                BackgroundColor = panelBackgroundColor,
-                                BorderRadius = new Vector4(6f, 6f, 6f, 6f),
-                                BorderColor = Color.FromHex(0xff00ff, 1f),
-                                BorderSize = BorderSize.All(2f)
-                            }
-                        },
-                        new TextWidget("Hi")
-                        {
-                            Style = new TextStyle
-                            {
-                                Color = Color.FromHex(0xff00ff, 1f),
-                                HorizontalTextAlignment = TextAlignment.Center,
-                                VerticalTextAlignment = TextAlignment.Center
-                            }
-                        }    
-                    }
-                }
-            };
-        }
-    }
-
-    abstract class WidgetOld
-    {
-        private Rect m_ScreenRect;
-        public Rect ScreenRect
+        private readonly TextStyle m_TextStyleNormal = new()
         {
-            get => m_ScreenRect;
-            set => SetField(ref m_ScreenRect, value);
-        }
+            Color = Color.FromHex(0xf7f7f7, 1f),
+            VerticalTextAlignment = TextAlignment.Center,
+            HorizontalTextAlignment = TextAlignment.Center
+        };
+    
+        private readonly TextStyle m_TextStylePressed = new()
+        {
+            Color = Color.FromHex(0xc8c7c9, 1f),
+            VerticalTextAlignment = TextAlignment.Center,
+            HorizontalTextAlignment = TextAlignment.Center
+        };
         
-        private bool m_IsVisible;
-        public bool IsVisible
-        {
-            get => m_IsVisible;
-            set
-            {
-                if (m_IsVisible == value)
-                    return;
-                
-                m_IsVisible = value;
-                OnVisibilityStateChanged();
-            }
-        }
-
-        protected virtual void OnVisibilityStateChanged()
-        {
-            if (m_IsVisible)
-                OnBecameVisible();
-            else
-                OnBecameHidden();
-            SetDirty();
-        }
-        
-        protected abstract void OnBecameVisible();
-        protected abstract void OnBecameHidden();
-
         private bool m_IsHovered;
-        public bool IsHovered
+        private bool IsHovered
         {
             get => m_IsHovered;
             set => SetField(ref m_IsHovered, value);
         }
         
         private bool m_IsPressed;
-        public bool IsPressed
+        private bool IsPressed
         {
             get => m_IsPressed;
             set => SetField(ref m_IsPressed, value);
         }
         
-        private bool m_IsDirty;
-        public void Update()
+        protected override IWidget Build(IBuildContext context)
         {
-            if (m_IsDirty && m_IsVisible)
-                UpdateView();
-            m_IsDirty = false;
-        }
+            //Console.WriteLine("Build:TextButtonWidget");
 
-        protected bool SetField<T>(ref T field, T value)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value))
-                return false;
-
-            field = value;
-            SetDirty();
-            return true;
-        }
-        
-        protected void SetDirty()
-        {
-            m_IsDirty = true;
-        }
-
-        protected abstract void UpdateView();
-    }
-    
-    sealed class TextButtonOld : WidgetOld
-    {
-        private string m_Text;
-        public string Text
-        {
-            get => m_Text;
-            set => SetField(ref m_Text, value);
-        }
-
-        private TextStyle m_TextStyleNormal;
-        public TextStyle TextStyleNormal
-        {
-            get => m_TextStyleNormal;
-            set => SetField(ref m_TextStyleNormal, value);
-        }
-
-        private TextStyle m_TextStylePressedPressed;
-        public TextStyle TextStylePressed
-        {
-            get => m_TextStylePressedPressed;
-            set => SetField(ref m_TextStylePressedPressed, value);
-        }
-
-        private PanelStyle m_PanelStyleNormal;
-        public PanelStyle PanelStyleNormal
-        {
-            get => m_PanelStyleNormal;
-            set => SetField(ref m_PanelStyleNormal, value);
-        }
-        
-        private PanelStyle m_PanelStylePressed;
-        public PanelStyle PanelStylePressed
-        {
-            get => m_PanelStylePressed;
-            set => SetField(ref m_PanelStylePressed, value);
-        }
-        
-        private readonly ITextRenderer m_TextRenderer;
-        private readonly IPanelRenderer m_PanelRenderer;
-        
-        private IRenderedPanel? m_RenderedPanel;
-        private IRenderedText? m_RenderedText;
-
-        public TextButtonOld(IPanelRenderer panelRenderer, ITextRenderer textRenderer)
-        {
-            m_PanelRenderer = panelRenderer;
-            m_TextRenderer = textRenderer;
-        }
-
-        protected override void OnBecameVisible()
-        {
-            m_RenderedPanel = m_PanelRenderer.Render(ScreenRect, PanelStyleNormal);
-            m_RenderedText = m_TextRenderer.Render(Text, ScreenRect, TextStyleNormal);
-        }
-
-        protected override void OnBecameHidden()
-        {
-            m_RenderedPanel?.Dispose();
-            m_RenderedPanel = null;
-            
-            m_RenderedText?.Dispose();
-            m_RenderedText = null;
-        }
-
-        protected override void UpdateView()
-        {
-            Debug.Assert(m_RenderedPanel != null);
-            Debug.Assert(m_RenderedText != null);
-
-            var panelStyle = PanelStyleNormal;
-            var textStyle = TextStyleNormal;
+            var panelStyle = m_PanelStyleNormal;
+            var textStyle = m_TextStyleNormal;
             if (IsPressed)
             {
-                textStyle = TextStylePressed;
-                panelStyle = PanelStylePressed;
+                panelStyle = m_PanelStylePressed;
+                textStyle = m_TextStylePressed;
             }
             else if (IsHovered)
             {
+                panelStyle = m_PanelStyleHovered;
             }
-            
-            m_RenderedPanel.ScreenRect = ScreenRect;
-            m_RenderedPanel.Style = panelStyle;
-            
-            m_RenderedText.ScreenRect = ScreenRect;
-            m_RenderedText.Style = textStyle;
+
+            return new InputListenerWidget
+            {
+                ScreenRect = ScreenRect,
+                OnPointerEnter = () => IsHovered = true,
+                OnPointerExit = () => IsHovered = false,
+                OnPointerPressed = () => IsPressed = true,
+                OnPointerReleased = () => IsPressed = false,
+                Child = new StackWidget
+                {
+                    Children = new List<IWidget>
+                    {
+                        new PanelWidget
+                        {
+                            Style = panelStyle
+                        },
+                        new TextWidget("Hi")
+                        {
+                            Style = textStyle
+                        }    
+                    }
+                }
+            };
         }
     }
 }
