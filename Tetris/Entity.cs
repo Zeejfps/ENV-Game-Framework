a2464c94cfc2644b406cdf4a3e67a0e65b486faa
Zee Vasilyev <zeejfps@gmail.com>
2023-12-20 20:18:40 -0600

    Created an Entity Struct

---
+++ Tetris/Entity.cs
@@ -0,0 +1,43 @@
+﻿namespace Tetris;
+
+public struct Entity
+{
+    private readonly HashSet<string> m_Tags = new();
+
+    public Entity() {}
+
+    public void AddTag(string tag)
+    {
+        m_Tags.Add(tag);
+    }
+
+    public void RemoveTag(string tag)
+    {
+        m_Tags.Remove(tag);
+    }
+
+    public bool WithTags(params string[] tags)
+    {
+        foreach (var tag in tags)
+        {
+            if (!m_Tags.Contains(tag))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public bool WithoutTags(params string[] tags)
+    {
+        foreach (var tag in tags)
+        {
+            if (m_Tags.Contains(tag))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
