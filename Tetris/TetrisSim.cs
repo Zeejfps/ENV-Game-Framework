4c1d927fcec974f2610dae0e88a2a76a1c788035
Zee Vasilyev <zeejfps@gmail.com>
2023-12-30 10:06:56 -0600

    Trying something new

---
+++ Tetris/TetrisSim.cs
@@ -0,0 +1,39 @@
+﻿namespace Tetris;
+
+public sealed class TetrisSim
+{
+    public void Update(float dt)
+    {
+        
+    }
+
+    public void MoveTetrominoRight()
+    {
+        
+    }
+
+    public void MoveTetrominoLeft()
+    {
+        
+    }
+
+    public void MoveTetrominoDown()
+    {
+        
+    }
+
+    public void DropTetrominoInstantly()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        
+    }
+}
---
+++ Tetris/TetrisSimState.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace Tetris;
+
+public sealed class TetrisSimState
+{
+    public PlayState PlayState;
+    public TetrominoState TetrominoState;
+    public MonominoState[] StaticMonominoStates;
+}
+
+public struct MonominoState
+{
+    public Vector2 Position;
+    public TetrominoType Type;
+}
+
+public struct TetrominoState
+{
+    public Vector2 Position;
+    public TetrominoType Type;
+}
+
+public enum TetrominoType
+{
+    I, O, S, Z, L, J
+}
+
+public enum PlayState
+{
+    Paused,
+    Playing
+}
