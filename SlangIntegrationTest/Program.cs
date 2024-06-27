73662661176a64bc0930f55c226bf78f73c029b9
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-06-27 11:20:30 -0500

    got a module to load

---
+++ SlangIntegrationTest/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+using SlangIntegrationTest;
+
+unsafe
+{
+    var createGlobalSessionResult = SlangCompilerAPI.slang_createGlobalSession(0, out var globalSession);
+    Console.WriteLine($"Result: {createGlobalSessionResult}");
+
+
+    var searchPaths = new[]
+    {
+        "./Shaders"
+    };
+    
+    // Allocate an array of pointers
+    IntPtr[] pointerArray = new IntPtr[searchPaths.Length];
+
+    // Allocate and marshal each string
+    for (int i = 0; i < searchPaths.Length; i++)
+    {
+        pointerArray[i] = Marshal.StringToHGlobalAnsi(searchPaths[i]);
+    }
+
+    // Allocate memory for the array of pointers
+    IntPtr result = Marshal.AllocHGlobal(IntPtr.Size * pointerArray.Length);
+    Marshal.Copy(pointerArray, 0, result, pointerArray.Length);
+
+    var desc = new SlangCompilerAPI.SessionDesc
+    {
+        SearchPaths = result,
+        SearchPathCount = searchPaths.Length
+    };
+    var createSessionResult = globalSession.CreateSession(desc, out var session);
+    Console.WriteLine(createSessionResult);
+
+    Console.WriteLine(session.GetGlobalSession() == globalSession);
+
+    var name = Marshal.StringToHGlobalAnsi("hello-world.slang");
+    var module = session.LoadModule(name, out var blob);
+
+    if (blob != null)
+    {
+        //
+        Console.WriteLine($"Blob size: {blob.GetBufferSize()}");
+        //
+        var str = Marshal.PtrToStringAnsi(blob.GetBufferPointer(), blob.GetBufferSize());
+        Console.WriteLine(str);
+    }
+    
+}
---
+++ SlangIntegrationTest/SlangCompilerAPI.cs
@@ -0,0 +1,123 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SlangIntegrationTest;
+
+using SlangResult = Int32;
+using SlangInt = Int64;
+
+public static class SlangCompilerAPI
+{
+    [ComImport]
+    [Guid("00000000-0000-0000-C000-000000000046")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ISlangUnknown
+    {
+        // IUnknown methods
+        [PreserveSig]
+        int QueryInterface(ref Guid riid, out IntPtr ppvObject);
+        
+        [PreserveSig]
+        uint AddRef();
+        
+        [PreserveSig]
+        uint Release();
+    }
+    
+    [ComImport]
+    [Guid("c140b5fd-0c78-452e-ba7c-1a1e70c7f71c")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IGlobalSession : ISlangUnknown
+    {
+        SlangResult CreateSession(
+            [In] in SessionDesc desc,
+            [Out] out ISession session);
+    }
+    
+    [ComImport]
+    [Guid("67618701-d116-468f-ab3b-474bedce0e3d")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ISession : ISlangUnknown
+    {
+        [PreserveSig]
+        IGlobalSession GetGlobalSession();
+        
+        IModule LoadModule(
+            [In] IntPtr moduleName,
+            [Out] out ISlangBlob? outDiagnostics);
+    }
+    
+    [ComImport]
+    [Guid("5bc42be8-5c50-4929-9e5e-d15e7c24015f")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IComponentType : ISlangUnknown
+    {
+        
+    }
+    
+    [ComImport]
+    [Guid("0c720e64-8722-4d31-8990-638a98b1c279")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IModule : IComponentType
+    {
+        
+    }
+    
+    [ComImport]
+    [Guid("8BA5FB08-5195-40e2-AC58-0D989C3A0102")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ISlangBlob : ISlangUnknown
+    {
+        [PreserveSig]
+        IntPtr GetBufferPointer();
+        
+        [PreserveSig]
+        int GetBufferSize();
+    }
+    
+    public struct SessionDesc
+    {
+        private int StructureSize;
+
+        public IntPtr Targets;
+        public SlangInt TargetCount;
+        
+        public SessionFlags flags = SessionFlags.kSessionFlags_None;
+
+        public SlangMatrixLayoutMode DefaultMatrixLayoutMode = SlangMatrixLayoutMode.SLANG_MATRIX_LAYOUT_ROW_MAJOR;
+
+        public IntPtr SearchPaths;
+        public SlangInt SearchPathCount;
+        
+        public IntPtr PreprocessorMacros;
+        public SlangInt PreprocessorMacroCount;
+        
+        public IntPtr FileSystem;
+        
+        public bool EnableEffectAnnotations;
+        public bool AllowGLSLSyntax;
+
+        public IntPtr CompilerOptionEntries;
+        public UInt32 CompilerOptionEntryCount;
+
+        public SessionDesc()
+        {
+            StructureSize = Marshal.SizeOf<SessionDesc>();
+        }
+    }
+    
+    public enum SlangMatrixLayoutMode : uint
+    {
+        SLANG_MATRIX_LAYOUT_MODE_UNKNOWN = 0,
+        SLANG_MATRIX_LAYOUT_ROW_MAJOR,
+        SLANG_MATRIX_LAYOUT_COLUMN_MAJOR,
+    }
+    
+    public enum SessionFlags
+    {
+        kSessionFlags_None = 0
+    }
+    
+    [DllImport("slang.dll")]
+    public static extern SlangResult slang_createGlobalSession(SlangInt apiVersion, out IGlobalSession outGlobalSession);
+}
