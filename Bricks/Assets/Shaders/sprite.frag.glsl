ab71091a0faa495a00846280f31225a362fc3eec
Zee Vasilyev <zeejfps@gmail.com>
2024-07-02 21:53:25 -0500

    we have textures

--- Bricks/Assets/Shaders/sprite.frag.glsl
+++ Bricks/Assets/Shaders/sprite.frag.glsl
@@ -1,9 +1,12 @@
 ﻿#version 460
 
-in vec4 f_uvCoords;
+in vec2 f_uvCoords;
+
+uniform sampler2D tex;
 
 out vec4 f_Color;
 
 void main() {
-    f_Color = vec4(f_uvCoords.rgb, 1);
+    //* vec2(0.1171875, 0.0390625 + 0.078125)
+    f_Color = texture(tex, f_uvCoords.rg);
 }
--- Bricks/Assets/Shaders/sprite.vert.glsl
+++ Bricks/Assets/Shaders/sprite.vert.glsl
@@ -6,7 +6,7 @@ layout(location = 2) in vec4 v_ScreenRect;
 
 uniform mat4 projection_matrix;
 
-out vec4 f_uvCoords;
+out vec2 f_uvCoords;
 
 void main() {
     float rectX = v_ScreenRect.x;
@@ -21,7 +21,7 @@ void main() {
     position.x = (position.x * rectHalfWidth + rectHalfWidth) + rectX;
     position.y = (position.y * rectHalfHeight + rectHalfHeight) + rectY;
 
-    f_uvCoords = v_Normals;
+    f_uvCoords = vec2(v_Normals.x * 0.1171875, v_Normals.y * 0.0390625 + 0.0390625);
     
     gl_Position = projection_matrix * position;
 }
