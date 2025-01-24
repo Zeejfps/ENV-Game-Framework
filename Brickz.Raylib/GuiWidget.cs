72028a5fb42e4191a719d773a4d3b468a8e28d69
Zee Vasilyev <zeejfps@gmail.com>
2025-01-24 16:41:53 -0600

    Switched to using my UI

--- Brickz.Raylib/GuiWidget.cs
+++ Brickz.Raylib/GuiWidget.cs
@@ -6,7 +6,7 @@ namespace Bricks.RaylibBackend;
 public sealed class GuiWidget : StatefulWidget
 {
     private readonly BrickzGame _game;
-
+    
     public GuiWidget(BrickzGame game)
     {
         _game = game;
@@ -16,50 +16,116 @@ public sealed class GuiWidget : StatefulWidget
         };
         ScreenRect = new Rect(0, 0, 640, 480);
     }
+
+    private IWidget DimmerWidget(IBuildContext context)
+    {
+        return new PanelWidget
+        {
+            Style = new PanelStyle
+            {
+                BackgroundColor = new Color(0f, 0f, 0f, 0.75f),
+            }
+        };
+    }
     
     protected override IWidget BuildContent(IBuildContext context)
     {
-        if (_game.State == GameState.Defeat)
+        if (_game.State == GameState.Playing)
+            return this;
+        
+        IWidget panel;
+        if (_game.State == GameState.Paused)
         {
-            return new StackWidget
+            panel = new Column
             {
+                Spacing = 10,
+                MainAxisSize = MainAxisSize.Min,
+                MainAxisAlignment = MainAxisAlignment.Center,
+                CrossAxisSize = CrossAxisSize.Min,
+                CrossAxisAlignment = CrossAxisAlignment.Center,
                 Children =
                 {
-                    new PanelWidget
+                    new TextWidget("PAUSED")
                     {
-                        Style = new PanelStyle
+                        Style = new TextStyle
                         {
-                            BackgroundColor = new Color(0f, 0f, 0f, 0.75f),
+                            FontScale = 50,
+                            Color = new Color(0f, 1f, 0f, 1f),
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            VerticalTextAlignment = TextAlignment.Center,
                         }
                     },
-                    new Column
+                    new TextButton("resume")
                     {
-                        Spacing = 10,
-                        MainAxisSize = MainAxisSize.Min,
-                        MainAxisAlignment = MainAxisAlignment.Center,
-                        CrossAxisSize = CrossAxisSize.Min,
-                        CrossAxisAlignment = CrossAxisAlignment.Center,
-                        Children =
+                        OnClicked = _game.Resume
+                    },
+                }
+            };
+        } 
+        else if (_game.State == GameState.Victory)
+        {
+            panel = new Column
+            {
+                Spacing = 10,
+                MainAxisSize = MainAxisSize.Min,
+                MainAxisAlignment = MainAxisAlignment.Center,
+                CrossAxisSize = CrossAxisSize.Min,
+                CrossAxisAlignment = CrossAxisAlignment.Center,
+                Children =
+                {
+                    new TextWidget("VICTORY!")
+                    {
+                        Style = new TextStyle
                         {
-                            new TextWidget("DEFEAT :(")
-                            {
-                                Style = new TextStyle
-                                {
-                                    FontScale = 50,
-                                    Color = new Color(1f, 0f, 0f, 1f),
-                                    HorizontalTextAlignment = TextAlignment.Center,
-                                    VerticalTextAlignment = TextAlignment.Center,
-                                }
-                            },
-                            new TextButton("restart")
-                            {
-                                OnClicked = _game.Restart
-                            },
+                            FontScale = 50,
+                            Color = new Color(0f, 1f, 0f, 1f),
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            VerticalTextAlignment = TextAlignment.Center,
                         }
-                    }
+                    },
+                    new TextButton("restart")
+                    {
+                        OnClicked = _game.Restart
+                    },
                 }
             };
         }
-        return this;
+        else
+        {
+            panel = new Column
+            {
+                Spacing = 10,
+                MainAxisSize = MainAxisSize.Min,
+                MainAxisAlignment = MainAxisAlignment.Center,
+                CrossAxisSize = CrossAxisSize.Min,
+                CrossAxisAlignment = CrossAxisAlignment.Center,
+                Children =
+                {
+                    new TextWidget("DEFEAT :(")
+                    {
+                        Style = new TextStyle
+                        {
+                            FontScale = 50,
+                            Color = new Color(1f, 0f, 0f, 1f),
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            VerticalTextAlignment = TextAlignment.Center,
+                        }
+                    },
+                    new TextButton("restart")
+                    {
+                        OnClicked = _game.Restart
+                    },
+                }
+            };
+        }
+
+        return new StackWidget
+        {
+            Children =
+            {
+                DimmerWidget(context),
+                panel
+            }
+        };
     }
 }
--- Brickz.Raylib/RaylibFramework.cs
+++ Brickz.Raylib/RaylibFramework.cs
@@ -71,73 +71,11 @@ internal sealed class RaylibFramework : IFramework
         {
             DrawBrick(brick);
         }
-
-        if (_game.State == GameState.Victory)
-        {
-            DrawVictoryScreen();
-        }
-        else if (_game.State == GameState.Paused)
-        {
-            DrawPauseScreen();
-        }
-        else if (_game.State == GameState.Defeat)
-        {
-            //DrawDefeatScreen();
-        }
         
         _gui.Update(_guiRenderer);
-        
         _guiRenderer.Render();
         
         Raylib.EndDrawing();
-    }
-
-    private void DrawDefeatScreen()
-    {
-        var victoryText = "DEFEAT :(";
-        var fontSize = 50;
-        Raylib.DrawRectangle(0, 0, 640, 480, new Color(0, 0, 0, 200));
-        var width = Raylib.MeasureText(victoryText, fontSize);
-        Raylib.DrawText(victoryText, (int)(320 - width * 0.5f), 180, fontSize, new Color(255, 50, 0, 255));
-        
-        var buttonPosition = new Rectangle(320 - 100, 250, 200, 40);
-        var restartButtonClicked = RayGui.GuiButton(buttonPosition, "restart");
-        if (restartButtonClicked)
-        {
-            _game.Restart();
-        }
-    }
-
-    private void DrawVictoryScreen()
-    {
-        var victoryText = "VICTORY!";
-        var fontSize = 50;
-        Raylib.DrawRectangle(0, 0, 640, 480, new Color(0, 0, 0, 200));
-        var width = Raylib.MeasureText(victoryText, fontSize);
-        Raylib.DrawText(victoryText, (int)(320 - width * 0.5f), 180, fontSize, new Color(0, 255, 0, 255));
-        
-        var buttonPosition = new Rectangle(320 - 100, 250, 200, 40);
-        var restartButtonClicked = RayGui.GuiButton(buttonPosition, "restart");
-        if (restartButtonClicked)
-        {
-            _game.Restart();
-        }
-    }
-
-    private void DrawPauseScreen()
-    {
-        var text = "PAUSED";
-        var fontSize = 50;
-        Raylib.DrawRectangle(0, 0, 640, 480, new Color(0, 0, 0, 200));
-        var width = Raylib.MeasureText(text, fontSize);
-        Raylib.DrawText(text, (int)(320 - width * 0.5f), 180, fontSize, new Color(0, 255, 0, 255));
-        
-        var buttonPosition = new Rectangle(320 - 100, 250, 200, 40);
-        var buttonClicked = RayGui.GuiButton(buttonPosition, "resume");
-        if (buttonClicked)
-        {
-            _game.Resume();
-        }
     }
 
     private void DrawRectangle(AABB rect, Color color)
