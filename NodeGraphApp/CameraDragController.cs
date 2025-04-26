387e9000298c71fdeafd24c2be16f20e81820872
Evgeny Vasilyev <zeejfps@gmail.com>
2025-04-26 15:44:28 -0500

    Switched to using CoorindateUtils

--- NodeGraphApp/CameraDragController.cs
+++ NodeGraphApp/CameraDragController.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using GLFW;
-using NodeGraphApp;
+
+namespace NodeGraphApp;
 
 public sealed class CameraDragController
 {
@@ -29,6 +30,7 @@ public sealed class CameraDragController
         var window = _window;
         var mouse = _mouse;
         var keyboard = _keyboard;
+        var camera = _camera;
 
         var canStartDragging = mouse.WasButtonPressedThisFrame(MouseButton.Left) &&
                                keyboard.IsKeyPressed(Keys.LeftAlt);
@@ -37,7 +39,7 @@ public sealed class CameraDragController
         if (canStartDragging)
         {
             _isDragging = true;
-            _prevCursorPosition = mouse.Position;
+            _prevCursorPosition = CoordinateUtils.ScreenToCameraViewPoint(window, camera, mouse.Position);
         }
 
         if (_isDragging)
@@ -49,26 +51,10 @@ public sealed class CameraDragController
                 return;
             }
 
-            Glfw.GetWindowSize(window, out var windowWidth, out var windowHeight);
-
-            var camera = _camera;
-            var newCursorScreenPosition = mouse.Position;
+            var newCursorScreenPosition = CoordinateUtils.ScreenToCameraViewPoint(window, camera, mouse.Position);
             var delta = newCursorScreenPosition - _prevCursorPosition;
             _prevCursorPosition = newCursorScreenPosition;
-
-            var ndcCoords = new Vector4
-            {
-                X = -delta.X / windowWidth,
-                Y = delta.Y / windowHeight,
-                Z = 0,
-                W = 0
-            };
-
-            Matrix4x4.Invert(camera.ProjectionMatrix, out var invProj);
-
-            var worldDelta = Vector4.Transform(ndcCoords, invProj);
-            var cameraDelta = new Vector2(worldDelta.X, worldDelta.Y);
-            camera.Position += cameraDelta * 2f;
+            camera.Position -= delta;
         }
     }
 }
