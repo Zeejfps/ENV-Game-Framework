c5f392b2ea7abf944f9e2579db096600644dae49
Evgeny Vasilyev <zeejfps@gmail.com>
2025-04-25 21:37:40 -0500

    So much effort just to get glfw to run on mac X_X

---
+++ NodeGraphApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using GLFW;
+using Monitor = GLFW.Monitor;
+
+Glfw.Init();
+
+var window = Glfw.CreateWindow(1280, 720, "Node Graph", Monitor.None, Window.None);
+
+Glfw.ShowWindow(window);
+
+Glfw.Terminate();
