cf9de8ac24474614534d636975b0a4782898b1df
Zee Vasilyev <zeejfps@gmail.com>
2025-04-28 10:20:08 -0500

    ok, getting closer

--- NodeGraphApp/Assets/Shaders/Glyph/glyph_vert.glsl
+++ NodeGraphApp/Assets/Shaders/Glyph/glyph_vert.glsl
@@ -10,6 +10,9 @@ out vec2 TexCoords;
 
 void main() {
     TexCoords = attr_Position;
+    TexCoords.x = TexCoords.x * u_spriteRect.z + u_spriteRect.x;
+    TexCoords.y = (1.0 - TexCoords.y) * u_spriteRect.w + u_spriteRect.y;
+    
     vec2 scaledPosition = u_rect.xy + attr_Position * u_rect.zw;
     vec4 position = vec4(scaledPosition.x, scaledPosition.y, 0, 1);
     gl_Position = u_viewProjMat * position;
