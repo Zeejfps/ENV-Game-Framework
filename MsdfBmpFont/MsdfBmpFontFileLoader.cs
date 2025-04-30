a3d56e53343c878a04b8a1d7553673a8e0bd1c56
Evgeny Vasilyev <zeejfps@gmail.com>
2025-04-29 22:30:40 -0500

    Made font loader to also not automatically resolve path

--- MsdfBmpFont/MsdfBmpFontFileLoader.cs
+++ MsdfBmpFont/MsdfBmpFontFileLoader.cs
@@ -6,9 +6,7 @@ public sealed class MsdfBmpFontFileLoader
 {
     public MsdfFontFile LoadFromFilePath(string pathToFile)
     {
-        var appPath = AppContext.BaseDirectory;
-        var fullFilePath = Path.Combine(appPath, pathToFile);
-        var fileContents = File.ReadAllText(fullFilePath);
+        var fileContents = File.ReadAllText(pathToFile);
         var fontData = JsonSerializer.Deserialize(fileContents, MsdfBmpFontJsonSerializationContext.Default.MsdfFontFile);
         if (fontData == null)
             throw new MsdfBmpFontLoadingException("Failed to deserialize font data");
@@ -17,9 +15,7 @@ public sealed class MsdfBmpFontFileLoader
 
     public async Task<MsdfFontFile> LoadFromFilePathAsync(string pathToFile)
     {
-        var appPath = AppContext.BaseDirectory;
-        var fullFilePath = Path.Combine(appPath, pathToFile);
-        var fileContents = await File.ReadAllTextAsync(fullFilePath);
+        var fileContents = await File.ReadAllTextAsync(pathToFile);
         var fontData = JsonSerializer.Deserialize(fileContents, MsdfBmpFontJsonSerializationContext.Default.MsdfFontFile);
         if (fontData == null)
             throw new MsdfBmpFontLoadingException("Failed to deserialize font data");
