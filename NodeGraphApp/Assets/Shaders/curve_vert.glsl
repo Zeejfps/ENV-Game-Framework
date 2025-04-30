3515bee0908acbf7d19eb99a627655bf4abae550
Evgeny Vasilyev <zeejfps@gmail.com>
2025-04-29 22:37:27 -0500

    finished curve vert shader

--- NodeGraphApp/Assets/Shaders/curve_vert.glsl
+++ NodeGraphApp/Assets/Shaders/curve_vert.glsl
@@ -5,5 +5,12 @@ layout(location = 0) in float t;
 uniform vec2 u_p0, u_p1, u_p2, u_p3;
 
 void main() {
+    float u = 1.0 - t;
+    vec2 pos =
+        u*u*u * u_p0 +
+        3*u*u*t * p1 +
+        3*u*t*t * p2 +
+        t*t*t * p3;
 
+    gl_Position = vec4(pos, 0.0, 1.0);
 }
