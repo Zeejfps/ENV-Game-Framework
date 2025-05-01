ab81fad07a11293cb4f3c3f2bbdf3ff9b7e872a1
Evgeny Vasilyev <zeejfps@gmail.com>
2025-04-30 22:19:59 -0500

    Removed unused command

--- build-linux-x64.sh
+++ build-linux-x64.sh
@@ -6,11 +6,10 @@ docker build -t linux-x64-builder \
     --build-arg PROJECT_PATH=./NodeGraphApp/NodeGraphApp.csproj \
     -f ./build-linux-x64.dockerfile .
 
-echo "Creating container..."
-docker create --name build-artifacts linux-x64-builder
-
 echo "Copying artifacts..."
-docker run --rm --name build-artifacts linux-x64-builder cp -r /publish /publish
+docker run --rm --name build-artifacts \
+    --platform=linux/amd64 \
+    linux-x64-builder cp -r /publish /publish
 
 
 
