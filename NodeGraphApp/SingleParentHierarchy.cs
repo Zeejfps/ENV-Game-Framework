405060dec69d16a75a7f256d341dbaac292c5a99
Zee Vasilyev <zeejfps@gmail.com>
2025-05-01 10:48:12 -0500

    Somewhat improved the child hierarchy

---
+++ NodeGraphApp/SingleParentHierarchy.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+
+public interface INode<T>
+{
+    T? Parent { get; set; }
+    
+    IList<T> Children { get; }
+}
+
+public sealed class SingleParentHierarchy<T> : IList<T> where T : class, INode<T>
+{
+    private readonly T _parent;
+    private readonly List<T> _children = new();
+
+    public SingleParentHierarchy(T parent)
+    {
+        _parent = parent;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _children.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T item)
+    {
+        if (item.Parent != null && item.Parent != _parent)
+            item.Parent.Children.Remove(item);
+        
+        item.Parent = _parent;
+        _children.Add(item);
+    }
+
+    public void Clear()
+    {
+        foreach (var child in _children)
+            child.Parent = null;
+        _children.Clear();
+    }
+
+    public bool Contains(T item)
+    {
+        return _children.Contains(item);
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        _children.CopyTo(array, arrayIndex);
+    }
+
+    public bool Remove(T child)
+    {
+        var removed = _children.Remove(child);
+        if (removed)
+            child.Parent = null;
+        return removed;
+    }
+
+    public int Count => _children.Count;
+    public bool IsReadOnly => false;
+    public int IndexOf(T child)
+    {
+        return _children.IndexOf(child);
+    }
+
+    public void Insert(int index, T child)
+    {
+        if (child.Parent != null && child.Parent != _parent)
+            child.Parent.Children.Remove(child);
+
+        child.Parent = _parent;
+        _children.Insert(index, child);
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index >= 0 && index < _children.Count)
+        {
+            var child = _children[index];
+            child.Parent = null;
+        }
+        _children.RemoveAt(index);
+    }
+
+    public T this[int index]
+    {
+        get => _children[index];
+        set
+        {
+            if (index >= 0 && index < _children.Count)
+            {
+                var child = _children[index];
+                child.Parent = null;
+            }
+
+            if (value.Parent != null && value.Parent != _parent)
+                value.Parent.Children.Remove(value);
+            
+            _children[index] = value;
+            value.Parent = _parent;
+        }
+    }
+}
--- NodeGraphApp/VisualNode.cs
+++ NodeGraphApp/VisualNode.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 using NodeGraphApp;
 using TextAlignment = NodeGraphApp.TextAlignment;
 
-public class VisualNode
+public class VisualNode : INode<VisualNode>
 {
     public Action<ScreenRect>? BoundsChanged;
 
@@ -47,101 +47,14 @@ public class VisualNode
     public TextAlignment TextVerticalAlignment { get; set; }
     public Vector2 CenterPosition => new(Bounds.Left + Bounds.Width*0.5f, Bounds.Bottom + Bounds.Height*0.5f);
 
+    VisualNode? INode<VisualNode>.Parent
+    {
+        get => Parent;
+        set => Parent = value;
+    }
+    
     public VisualNode()
     {
-        Children = new ChildrenList(this);
-    }
-
-    private sealed class ChildrenList : IList<VisualNode>
-    {
-        private readonly VisualNode _parent;
-        private readonly List<VisualNode> _children = new();
-
-        public ChildrenList(VisualNode parent)
-        {
-            _parent = parent;
-        }
-
-        public IEnumerator<VisualNode> GetEnumerator()
-        {
-            return _children.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public void Add(VisualNode item)
-        {
-            if (item.Parent != null && item.Parent != _parent)
-                item.Parent.Children.Remove(item);
-            
-            item.Parent = _parent;
-            _children.Add(item);
-        }
-
-        public void Clear()
-        {
-            foreach (var child in _children)
-                child.Parent = null;
-            _children.Clear();
-        }
-
-        public bool Contains(VisualNode item)
-        {
-            return _children.Contains(item);
-        }
-
-        public void CopyTo(VisualNode[] array, int arrayIndex)
-        {
-            _children.CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(VisualNode child)
-        {
-            var removed = _children.Remove(child);
-            if (removed)
-                child.Parent = null;
-            return removed;
-        }
-
-        public int Count => _children.Count;
-        public bool IsReadOnly => false;
-        public int IndexOf(VisualNode child)
-        {
-            return _children.IndexOf(child);
-        }
-
-        public void Insert(int index, VisualNode child)
-        {
-            child.Parent = _parent;
-            _children.Insert(index, child);
-        }
-
-        public void RemoveAt(int index)
-        {
-            if (index >= 0 && index < _children.Count)
-            {
-                var child = _children[index];
-                child.Parent = null;
-            }
-            _children.RemoveAt(index);
-        }
-
-        public VisualNode this[int index]
-        {
-            get => _children[index];
-            set
-            {
-                if (index >= 0 && index < _children.Count)
-                {
-                    var child = _children[index];
-                    child.Parent = null;
-                }
-                _children[index] = value;
-                value.Parent = _parent;
-            }
-        }
+        Children = new SingleParentHierarchy<VisualNode>(this);
     }
 }
