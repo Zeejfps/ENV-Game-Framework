abf50afe99d103b55a7e489db33acc4a780c538f
Zee Vasilyev <zeejfps@gmail.com>
2025-07-10 15:31:58 -0500

    Creating a simple software renderer module

---
+++ SoftwareRendererModule/Bitmap.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SoftwareRendererModule;
+
+public sealed class Bitmap
+{
+    public int Width { get; }
+    public int Height { get; }
+    
+    private readonly int[] _pixels;
+
+    public Bitmap(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        _pixels = new int[width * height];
+    }
+
+    public void Fill(int color)
+    {
+        Array.Fill(_pixels, color);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public void SetPixel(int x, int y, int color)
+    {
+        var index = GetIndex(x, y);
+        _pixels[index] = color;
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public void SetPixel(int index, int color)
+    {
+        _pixels[index] = color;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public int GetPixel(int x, int y)
+    {
+        var index = GetIndex(x, y);
+        return _pixels[index];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    private int GetIndex(int x, int y)
+    {
+        return Width * y + x;
+    }
+}
---
+++ SoftwareRendererModule/Graphics.cs
@@ -0,0 +1,99 @@
+﻿namespace SoftwareRendererModule;
+
+public static class Graphics
+{
+    public static void DrawLineH(Bitmap bitmap, int x0, int y1, int width, int color)
+    {
+        var sx = x0;
+        if (sx < 0)
+            sx = 0;
+        
+        var ex = x0 + width;
+        if (ex > bitmap.Width)
+            ex = bitmap.Width;
+
+        for (var x = sx; x < ex; x++)
+        {
+            bitmap.SetPixel(x, y1, color);
+        }
+    }
+    
+    public static void DrawLineV(Bitmap bitmap, int x0, int y0, int height, int color)
+    {
+        var sy = y0;
+        if (sy < 0)
+            sy = 0;
+        
+        var ey = y0 + height;
+        if (ey > bitmap.Height)
+            ey = bitmap.Height;
+
+        for (var y = sy; y < ey; y++)
+        {
+            bitmap.SetPixel(x0, y, color);
+        }
+    }
+    
+    public static void DrawLine(Bitmap bitmap, int x0, int y0, int x1, int y1, int color)
+    {
+        if (y0 == y1)
+        {
+            var x = x0;
+            var width = x1 - x0;
+            if (x > x1)
+            {
+                x = x1;
+                width = x0 - x1;
+            }
+            
+            DrawLineH(bitmap, x, y0, width, color);
+            return;
+        }
+
+        if (x0 == x1)
+        {
+            var y = y0;
+            var height = y1 - y0;
+            if (height < 0)
+            {
+                y = y1;
+                height = -height;
+            }
+            
+            DrawLineV(bitmap, x0, y, height, color);
+            return;
+        }
+        
+        var dx = Math.Abs(x1 - x0);
+        var dy = Math.Abs(y1 - y0);
+
+        var sx = x0 < x1 ? 1 : -1;
+        var sy = y0 < y1 ? 1 : -1;
+
+        var err = dx - dy;
+
+        while (true)
+        {
+            if (x0 >= 0 && x0 < bitmap.Width && y0 >= 0 && y0 < bitmap.Height)
+            {
+                bitmap.SetPixel(x0, y0, color);
+            }
+
+            if (x0 == x1 && y0 == y1) break;
+
+            var e2 = 2 * err;
+
+            if (e2 > -dy)
+            {
+                err -= dy;
+                x0 += sx;
+            }
+
+            if (e2 < dx)
+            {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+}
