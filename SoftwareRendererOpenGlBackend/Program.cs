a3b53c4e91e23758ce985a2aa9f6bfc10491c77e
Zee Vasilyev <zeejfps@gmail.com>
2025-07-11 15:45:24 -0500

    In theory got the bitmap copied over to the texture now

--- SoftwareRendererOpenGlBackend/Program.cs
+++ SoftwareRendererOpenGlBackend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using GLFW;
+using SoftwareRendererModule;
 using static GL46;
 using static OpenGLSandbox.OpenGlUtils;
 using Monitor = GLFW.Monitor;
@@ -14,8 +15,9 @@ unsafe
     var windowWidth = 1280;
     var windowHeight = 720;
     var windowAspectRatio = (float)windowWidth / windowHeight;
-    var windowHandle = Glfw.CreateWindow(windowWidth, windowHeight, "Node Graph", Monitor.None, Window.None);
+    var windowHandle = Glfw.CreateWindow(windowWidth, windowHeight, "Software Renderer", Monitor.None, Window.None);
 
+    
     Glfw.MakeContextCurrent(windowHandle);
     Glfw.ShowWindow(windowHandle);
     Glfw.SwapInterval(1);
@@ -33,13 +35,20 @@ unsafe
     glActiveTexture(GL_TEXTURE0);
     AssertNoGlError();
     
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, (int)GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, (int)GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, (int)GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, (int)GL_NEAREST);
     AssertNoGlError();
 
+    var colorBuffer = new Bitmap(640, 480);
+
+    fixed (void* ptr = &colorBuffer.Pixels[0])
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, (int)GL_RGBA8, 
+            colorBuffer.Width, colorBuffer.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, ptr);
+        AssertNoGlError();
+    }
 
     while (!Glfw.WindowShouldClose(windowHandle))
     {
