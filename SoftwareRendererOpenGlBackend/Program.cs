dc91fe33d86511c3f2dfde6539dc0ded0ed69bd3
Zee Vasilyev <zeejfps@gmail.com>
2025-07-10 22:13:42 -0500

    Started work on a software renderer backend

---
+++ SoftwareRendererOpenGlBackend/Program.cs
@@ -0,0 +1,31 @@
+﻿using GLFW;
+using static GL46;
+using Monitor = GLFW.Monitor;
+
+Glfw.Init();
+
+Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+Glfw.WindowHint(Hint.ContextVersionMajor, 4);
+Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+
+var windowWidth = 1280;
+var windowHeight = 720;
+var windowAspectRatio = (float)windowWidth / windowHeight;
+var windowHandle = Glfw.CreateWindow(windowWidth, windowHeight, "Node Graph", Monitor.None, Window.None);
+
+Glfw.MakeContextCurrent(windowHandle);
+Glfw.ShowWindow(windowHandle);
+Glfw.SwapInterval(1);
+
+Import(Glfw.GetProcAddress);
+
+while (!Glfw.WindowShouldClose(windowHandle))
+{
+    Glfw.PollEvents();
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+            
+    Glfw.SwapBuffers(windowHandle);
+}
+
+Glfw.Terminate();
