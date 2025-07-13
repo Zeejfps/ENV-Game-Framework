c90128a285b215429b4c894ca2a65206f0b9cc14
Evgeny Vasilyev <zeejfps@gmail.com>
2025-07-13 10:40:17 -0500

    Fixed the errors

--- SoftwareRendererOpenGlBackend/Program.cs
+++ SoftwareRendererOpenGlBackend/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GLFW;
 using OpenGL.NET;
 using SoftwareRendererModule;
+using SoftwareRendererOpenGlBackend;
 using static GL46;
 using static OpenGLSandbox.OpenGlUtils;
 using static OpenGL.NET.GLBuffer;
@@ -25,77 +26,13 @@ unsafe
 
     Import(Glfw.GetProcAddress);
     AssertNoGlError();
-    
-    var colorBuffer = new Bitmap(320, 240);
 
-    var texture = new Texture2DBuilder()
-        .WithMinFilter(TextureMinFilter.Nearest)
-        .WithMagFilter(TextureMagFilter.Nearest)
-        .BindAndBuild();
-    AssertNoGlError();
-    
-    glTexImage2D<uint>(texture, 0,  GL_RGBA8, colorBuffer.Width, colorBuffer.Height, 
-        GL_RGBA, GL_UNSIGNED_BYTE, colorBuffer.Pixels);
-    AssertNoGlError();
+    var renderer = new Renderer();
 
-    var shaderProgram = new ShaderProgramCompiler()
-        .WithVertexShader("Assets/tex.vert.glsl")
-        .WithFragmentShader("Assets/tex.frag.glsl")
-        .Compile();
-    
-    float[] vertices =
-    {
-        // Positions        // Texture Coords
-        1.0f,  1.0f, 0.0f,  1.0f, 1.0f, // top right
-        1.0f, -1.0f, 0.0f,  1.0f, 0.0f, // bottom right
-        -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, // bottom left
-        -1.0f,  1.0f, 0.0f,  0.0f, 1.0f  // top left
-    };
-
-    uint[] indices =
-    {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
-    
-    uint vbo, vao, ibo;
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ibo);
-
-    glBindVertexArray(vao);
-
-    var vertexDataBuffer = glBindBuffer<float>(GL_ARRAY_BUFFER, vbo);
-    glBufferData(vertexDataBuffer, vertices, BufferUsageHint.StaticDraw);
-    AssertNoGlError();
-
-    glVertexAttribPointer<float>(
-        attribIndex: 0,
-        count: 3,
-        stride: 5,
-        offset: 0
-    );
-    AssertNoGlError();
-
-    glVertexAttribPointer<float>(
-        attribIndex: 1,
-        count: 2,
-        stride: 5,
-        offset: 3
-    );
-    AssertNoGlError();
-
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-
-    var indexDataBuffer = glBindBuffer<uint>(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(indexDataBuffer, indices, BufferUsageHint.StaticDraw);
-    
     SizeCallback windowSizeCallback = (window, width, height) =>
     {
         glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        Render();
+        renderer.Render();
         Glfw.SwapBuffers(window);
     };
     
@@ -105,40 +42,9 @@ unsafe
     while (!Glfw.WindowShouldClose(windowHandle))
     {
         Glfw.PollEvents();
-        
-        Render();
-
+        renderer.Render();
         Glfw.SwapBuffers(windowHandle);
     }
-    
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &vbo);
-    glDeleteBuffers(1, &ibo);
-    glDeleteProgram(shaderProgram.Id);
 
-    var textureId = texture.Id;
-    glDeleteTextures(1, &textureId);
     Glfw.Terminate();
-
-    var myVao = vao;
-
-    void Render()
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-        colorBuffer.Fill(0x000000);
-        
-        Graphics.DrawRect(colorBuffer, 300, 200, 100, 150, 0xFF00FF);
-        Graphics.FillRect(colorBuffer, 0, 0, 100, 150, 0xFF00FF);
-        Graphics.DrawLine(colorBuffer, 0, 200, 100, 200, 0xFF00FF);
-        Graphics.DrawLine(colorBuffer, 50, 200, 50, 300, 0xFF00FF);
-        Graphics.DrawLine(colorBuffer, 100, 200, 150, 300, 0xFF00FF);
-        
-        fixed(void* pixelDataPtr = &colorBuffer.Pixels[0])
-            glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, colorBuffer.Width, colorBuffer.Height, GL_RGBA, GL_UNSIGNED_BYTE, pixelDataPtr);
-        
-        // Drawing the textured quad.
-        glUseProgram(shaderProgram.Id);
-        glBindVertexArray(myVao);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
-    }
 }
--- SoftwareRendererOpenGlBackend/Renderer.cs
+++ SoftwareRendererOpenGlBackend/Renderer.cs
@@ -1,5 +1,140 @@
-﻿namespace SoftwareRendererModule;
+﻿using OpenGL.NET;
+using SoftwareRendererModule;
+using static GL46;
+using static OpenGLSandbox.OpenGlUtils;
+using static OpenGL.NET.GLBuffer;
+using static OpenGL.NET.GLTexture;
 
-public sealed class Renderer
+namespace SoftwareRendererOpenGlBackend;
+
+public sealed unsafe class Renderer : IDisposable
 {
+    private readonly Bitmap _colorBuffer;
+    private readonly ShaderProgramInfo _shaderProgram;
+    private readonly uint _vao;
+    private readonly uint _vbo;
+    private readonly uint _ibo;
+    private readonly Texture _texture;
+
+    private bool _isDisposed;
+
+    public Renderer()
+    {
+        var colorBuffer = new Bitmap(320, 240);
+
+        var texture = new Texture2DBuilder()
+            .WithMinFilter(TextureMinFilter.Nearest)
+            .WithMagFilter(TextureMagFilter.Nearest)
+            .BindAndBuild();
+        AssertNoGlError();
+
+        glTexImage2D<uint>(texture, 0,  GL_RGBA8, colorBuffer.Width, colorBuffer.Height,
+            GL_RGBA, GL_UNSIGNED_BYTE, colorBuffer.Pixels);
+        AssertNoGlError();
+
+        _shaderProgram = new ShaderProgramCompiler()
+            .WithVertexShader("Assets/tex.vert.glsl")
+            .WithFragmentShader("Assets/tex.frag.glsl")
+            .Compile();
+
+        float[] vertices =
+        [
+            // Positions        // Texture Coords
+            1.0f,  1.0f, 0.0f,  1.0f, 1.0f, // top right
+            1.0f, -1.0f, 0.0f,  1.0f, 0.0f, // bottom right
+            -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, // bottom left
+            -1.0f,  1.0f, 0.0f,  0.0f, 1.0f  // top left
+        ];
+
+        uint[] indices =
+        [
+            0, 1, 3, // first triangle
+            1, 2, 3  // second triangle
+        ];
+
+        uint vbo, vao, ibo;
+        glGenVertexArrays(1, &vao);
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ibo);
+
+        glBindVertexArray(vao);
+
+        var vertexDataBuffer = glBindBuffer<float>(GL_ARRAY_BUFFER, vbo);
+        glBufferData(vertexDataBuffer, vertices, BufferUsageHint.StaticDraw);
+        AssertNoGlError();
+
+        glVertexAttribPointer<float>(
+            attribIndex: 0,
+            count: 3,
+            stride: 5,
+            offset: 0
+        );
+        AssertNoGlError();
+
+        glVertexAttribPointer<float>(
+            attribIndex: 1,
+            count: 2,
+            stride: 5,
+            offset: 3
+        );
+        AssertNoGlError();
+
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        var indexDataBuffer = glBindBuffer<uint>(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glBufferData(indexDataBuffer, indices, BufferUsageHint.StaticDraw);
+
+        _vao = vao;
+        _vbo = vbo;
+        _ibo = ibo;
+        _texture = texture;
+        _colorBuffer = colorBuffer;
+    }
+
+    public void Render()
+    {
+        if (_isDisposed)
+            throw new ObjectDisposedException(nameof(Renderer));
+
+        var colorBuffer = _colorBuffer;
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        colorBuffer.Fill(0x000000);
+
+        Graphics.DrawRect(colorBuffer, 300, 200, 100, 150, 0xFF00FF);
+        Graphics.FillRect(colorBuffer, 0, 0, 100, 150, 0xFF00FF);
+        Graphics.DrawLine(colorBuffer, 0, 200, 100, 200, 0xFF00FF);
+        Graphics.DrawLine(colorBuffer, 50, 200, 50, 300, 0xFF00FF);
+        Graphics.DrawLine(colorBuffer, 100, 200, 150, 300, 0xFF00FF);
+
+        fixed(void* pixelDataPtr = &colorBuffer.Pixels[0])
+            glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, colorBuffer.Width, colorBuffer.Height, GL_RGBA, GL_UNSIGNED_BYTE, pixelDataPtr);
+
+        // Drawing the textured quad.
+        glUseProgram(_shaderProgram.Id);
+        glBindVertexArray(_vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
+    }
+
+    public void Dispose()
+    {
+        if (_isDisposed)
+            return;
+
+        _isDisposed = true;
+
+        var vao = _vao;
+        var vbo = _vbo;
+        var ibo = _ibo;
+        var shaderProgram = _shaderProgram;
+
+        glDeleteVertexArrays(1, &vao);
+        glDeleteBuffers(1, &vbo);
+        glDeleteBuffers(1, &ibo);
+        glDeleteProgram(shaderProgram.Id);
+
+        var textureId = _texture.Id;
+        glDeleteTextures(1, &textureId);
+    }
 }
