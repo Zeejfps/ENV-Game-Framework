31b4d9b5c4b88ef131164406b4ccbeb179eedc53
Zee Vasilyev <zeejfps@gmail.com>
2025-07-14 12:14:29 -0500

    Added a test for Key press

--- SoftwareRendererOpenGlBackend/QuadTreeRendererApp.cs
+++ SoftwareRendererOpenGlBackend/QuadTreeRendererApp.cs
@@ -12,6 +12,7 @@ public sealed class QuadTreeRendererApp : OpenGlApp
     private readonly SizeCallback _framebufferSizeCallback;
     private readonly MouseButtonCallback _mouseButtonCallback;
     private readonly MouseCallback _cursorPositonCallback;
+    private readonly KeyCallback _keyCallback;
     
     public QuadTreeRendererApp(StartupConfig startupConfig) : base(startupConfig)
     {
@@ -26,18 +27,36 @@ public sealed class QuadTreeRendererApp : OpenGlApp
                 Left = 0,
                 Width = framebufferWidth,
                 Height = framebufferHeight
-            }, 6, maxDepth: 5)
+            }, 20, maxDepth: 6)
         );
 
         _framebufferSizeCallback = HandleFrameBufferSizeEvent;
         _mouseButtonCallback = HandleMouseButtonEvent;
         _cursorPositonCallback = HandleMouseMoveEvent;
-        
+        _keyCallback = HandleKeyEvent;
+
         Glfw.SetFramebufferSizeCallback(WindowHandle, _framebufferSizeCallback);
         Glfw.SetMouseButtonCallback(WindowHandle, _mouseButtonCallback);
         Glfw.SetCursorPositionCallback(WindowHandle, _cursorPositonCallback);
+        Glfw.SetKeyCallback(WindowHandle, _keyCallback);
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    }
+
+    private void HandleKeyEvent(Window window, Keys key, int scanCode, InputState state, ModifierKeys mods)
+    {
+        if (key != Keys.Space)
+            return;
+            
+        if (state != InputState.Press)
+            return;
+
+        for (var i = 0; i < 100000; i++)
+        {
+            var x = Random.Shared.Next(0, _renderer.FramebufferWidth);
+            var y = Random.Shared.Next(0, _renderer.FramebufferHeight);
+            _renderer.AddItemAt(x, y);
+        }
     }
     
     private void WindowToWorldPoint(Window window, double windowX, double windowY, out int worldX, out int worldY)
