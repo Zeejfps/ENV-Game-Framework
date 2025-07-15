32ce4d2798f0d86405c5b1119945e2c8ca7e488f
Zee Vasilyev <zeejfps@gmail.com>
2025-07-15 11:48:48 -0500

    Implementing the stylesheet a bit more

--- ZGF.Gui/StyleSheet.cs
+++ ZGF.Gui/StyleSheet.cs
@@ -4,6 +4,29 @@ namespace ZGF.Gui;
 
 public sealed class StyleSheet
 {
+    private readonly Dictionary<string, Style> _styleByIdLookup = new();
+    private readonly Dictionary<string, Style> _styleByClassLookup = new();
+    
+    public void AddStyleForId(string id, Style style)
+    {
+        _styleByIdLookup[id] = style;
+    }
+
+    public void RemoveStyleForId(string id)
+    {
+        _styleByIdLookup.Remove(id);
+    }
+    
+    public void AddStyleForClass(string classId, Style style)
+    {
+        _styleByClassLookup[classId] = style;
+    }
+    
+    public void RemoveStyleForClass(string classId)
+    {
+        _styleByClassLookup.Remove(classId);
+    }
+    
     public bool TryGetById(string? id, [NotNullWhen(true)] out Style? style)
     {
         if (string.IsNullOrEmpty(id))
@@ -11,8 +34,8 @@ public sealed class StyleSheet
             style = null;
             return false;
         }
-        style = null;
-        return false;
+        
+        return _styleByIdLookup.TryGetValue(id, out style);
     }
     
     public bool TryGetByClass(string? classId, [NotNullWhen(true)] out Style? style)
@@ -23,7 +46,6 @@ public sealed class StyleSheet
             return false;
         }
         
-        style = null;
-        return false;
+        return _styleByClassLookup.TryGetValue(classId, out style);
     }
 }
