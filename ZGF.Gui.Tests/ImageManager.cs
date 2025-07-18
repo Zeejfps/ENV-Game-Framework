16a7ba3a29d0ec58c5186c7690dcca812e4345ee
Evgeny Vasilyev <zeejfps@gmail.com>
2025-07-17 22:09:45 -0500

    Got the image uploading to the GPU

--- ZGF.Gui.Tests/ImageManager.cs
+++ ZGF.Gui.Tests/ImageManager.cs
@@ -1,15 +1,48 @@
+using OpenGL.NET;
 using PngSharp.Api;
+using static GL46;
+using static OpenGLSandbox.OpenGlUtils;
+using static OpenGL.NET.GLTexture;
 
 namespace ZGF.Gui.Tests;
 
+sealed class GpuImage
+{
+    public required uint TextureId { get; init; }
+    public required int Width { get; init; }
+    public required int Height { get; init; }
+}
+
 public sealed class ImageManager : IImageManager
 {
-    private readonly Dictionary<string, IDecodedPng> _imageByUriLookup = new();
+    private readonly Dictionary<string, GpuImage> _imageByUriLookup = new();
 
     public void LoadImage(string imageUri)
     {
         var png = Png.DecodeFromFile(imageUri);
-        _imageByUriLookup.Add(imageUri, png);
+
+        var texture = new Texture2DBuilder()
+            .WithMagFilter(TextureMagFilter.Nearest)
+            .WithMinFilter(TextureMinFilter.Nearest)
+            .BindAndBuild();
+
+        glTexImage2D<byte>(texture, 0,
+            GL_RGBA8,
+            png.Width,
+            png.Height,
+            GL_RGBA,
+            GL_BYTE,
+            png.PixelData);
+        AssertNoGlError();
+
+        var gpuImage = new GpuImage
+        {
+            TextureId = texture.Id,
+            Width = png.Width,
+            Height = png.Height,
+        };
+
+        _imageByUriLookup.Add(imageUri, gpuImage);
     }
 
     public int GetImageWidth(string imageUri)
