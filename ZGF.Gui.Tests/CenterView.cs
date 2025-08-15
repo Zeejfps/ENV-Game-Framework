d6140652549a9a3d00eec6c3e740a02561e858dd
Zee Vasilyev <zeejfps@gmail.com>
2025-08-15 13:55:11 -0500

    Fixed center view again

--- ZGF.Gui.Tests/CenterView.cs
+++ ZGF.Gui.Tests/CenterView.cs
@@ -7,10 +7,10 @@ public sealed class CenterView : View
     protected override void OnLayoutChild(in RectF position, View child)
     {
         var childSize = child.MeasureSelf();
-        var xOffset = (position.Width + childSize.Width) * 0.5f;
+        var xOffset = (position.Width - childSize.Width) * 0.5f;
         var yOffset = (position.Height - childSize.Height) * 0.5f;
-        child.LeftConstraint = xOffset;
-        child.BottomConstraint = yOffset;
+        child.LeftConstraint = position.Left + xOffset;
+        child.BottomConstraint = position.Bottom + yOffset;
         child.LayoutSelf();
     }
 }
