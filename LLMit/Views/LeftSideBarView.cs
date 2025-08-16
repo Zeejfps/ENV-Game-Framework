5c3621ba22a51413219cbe49f91a9d3101e80f65
Evgeny Vasilyev <zeejfps@gmail.com>
2025-08-16 14:00:07 -0500

    Added a Chat History label to teh left bar

--- LLMit/Views/LeftSideBarView.cs
+++ LLMit/Views/LeftSideBarView.cs
@@ -1,4 +1,5 @@
 using ZGF.Gui;
+using ZGF.Gui.Layouts;
 
 namespace LLMit.Views;
 
@@ -11,6 +12,22 @@ public sealed class LeftSideBarView : View
         var background = new RectView
         {
             BackgroundColor = 0xFF181818,
+            Padding = PaddingStyle.All(8),
+            Children =
+            {
+                new ColumnView
+                {
+                    Children =
+                    {
+                        new TextView
+                        {
+                            Text = "Chat History",
+                            TextColor = 0xFFFFFFFF,
+                            //HorizontalTextAlignment = TextAlignment.Center,
+                        }
+                    }
+                }
+            }
         };
 
         AddChildToSelf(background);
