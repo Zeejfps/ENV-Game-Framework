d87737a1ade7175450be19f70dd9ca69ffc91e78
zeejfps <zeejfps@gmail.com>
2025-09-14 17:23:29 -0500

    Created the world system

---
+++ ZGF.ECSModule/World.cs
@@ -0,0 +1,40 @@
+namespace ZGF.ECSModule;
+
+public sealed class WorldSystem<TEntity> : System
+{
+    private readonly HashSet<TEntity> _entities = new();
+    private readonly HashSet<TEntity> _entitiesToSpawn = new();
+    private readonly HashSet<TEntity> _entitiesToDespawn = new();
+    
+    public IEnumerable<TEntity> Entities => _entities;
+    public IEnumerable<TEntity> SpawningEntities => _entitiesToSpawn;
+    public IEnumerable<TEntity> DespawningEntities => _entitiesToDespawn;
+    
+    public void Spawn(TEntity entity)
+    {
+        _entitiesToSpawn.Add(entity);
+        _entitiesToDespawn.Remove(entity);
+    }
+
+    public void Despawn(TEntity entity)
+    {
+        _entitiesToDespawn.Add(entity);
+        _entitiesToSpawn.Remove(entity);
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+        foreach (var entity in _entitiesToSpawn)
+        {
+            _entities.Add(entity);
+        }
+        _entitiesToSpawn.Clear();
+        
+        foreach (var entity in _entitiesToDespawn)
+        {
+            _entities.Remove(entity);
+        }
+        _entitiesToDespawn.Clear();
+    }
+}
