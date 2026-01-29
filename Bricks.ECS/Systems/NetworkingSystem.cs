002978e17db1172f8a2d58ee8dc288ce03ec322c
Zee Vasilyev <zeejfps@gmail.com>
2026-01-29 08:30:30 -0600

    network system WIP

---
+++ Bricks.ECS/Systems/NetworkingSystem.cs
@@ -0,0 +1,48 @@
+﻿using Bricks.ECS.Components;
+using ZGF.ECSModule;
+
+namespace Bricks.ECS.Systems;
+
+public sealed class NetworkingSystem : SystemBase
+{
+    private readonly ComponentSystem<Entity, Sprite> _sprites;
+    private readonly ComponentSystem<Entity, Transform> _transforms;
+
+    public NetworkingSystem(ComponentSystem<Entity, Sprite> sprites, ComponentSystem<Entity, Transform> transforms)
+    {
+        _sprites = sprites;
+        _transforms = transforms;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach (var (entity, component) in _sprites.AddedComponents)
+        {
+            var message = new Message<Sprite>
+            {
+                Entity = entity,
+                Component = component,
+                ActionKind = ActionKind.Added,
+            };
+        }
+
+        foreach (var (entity, component) in _sprites.UpdatedComponents)
+        {
+            
+        }
+    }
+}
+
+public enum ActionKind
+{
+    Added,
+    Updated,
+    Removed,
+}
+
+public struct Message<T>
+{
+    public ActionKind ActionKind { get; set; }
+    public Entity Entity { get; set; }
+    public T Component { get; set; }
+}
