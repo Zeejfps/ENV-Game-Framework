587024a139ecbd008ae0953561900c1b3956fc86
Zee Vasilyev <zeejfps@gmail.com>
2026-05-13 23:34:38 -0500

    Created a FlexColumnView

--- GitGui/AppView.cs
+++ GitGui/AppView.cs
@@ -21,19 +21,21 @@ public sealed class OverlayView : View
     private IMessageBus? _messageBus;
 
     private readonly RectView _background;
-    
+    private readonly AddRepoDialog _dialog;
+    private bool _isOpen;
+
     public OverlayView()
     {
         _background = new RectView
         {
             BackgroundColor = 0,
         };
+        _dialog = new AddRepoDialog(Close);
         Children.Add(_background);
     }
 
     protected override void OnAttachedToContext(Context context)
     {
-        Console.WriteLine("Overlay view attached to context");
         _messageBus = context.Get<IMessageBus>();
         _messageBus?.Subscribe<AddRepoMessage>(OnAddRepoMessageReceived);
     }
@@ -46,8 +48,160 @@ public sealed class OverlayView : View
 
     private void OnAddRepoMessageReceived(AddRepoMessage obj)
     {
-        Console.WriteLine("Add repo message received");
-        _background.BackgroundColor = 0xFF00FF00;
+        Open();
+    }
+
+    private void Open()
+    {
+        if (_isOpen)
+            return;
+        _isOpen = true;
+        _background.BackgroundColor = 0xB0000000;
+        Children.Add(_dialog);
+    }
+
+    private void Close()
+    {
+        if (!_isOpen)
+            return;
+        _isOpen = false;
+        _background.BackgroundColor = 0;
+        Children.Remove(_dialog);
+    }
+
+    protected override void OnLayoutChildren()
+    {
+        var position = Position;
+        _background.LeftConstraint = position.Left;
+        _background.BottomConstraint = position.Bottom;
+        _background.MinWidthConstraint = position.Width;
+        _background.MaxWidthConstraint = position.Width;
+        _background.MaxHeightConstraint = position.Height;
+        _background.LayoutSelf();
+
+        if (!_isOpen)
+            return;
+
+        const float dialogWidth = 360f;
+        const float dialogHeight = 230f;
+        _dialog.LeftConstraint = position.Left + (position.Width - dialogWidth) * 0.5f;
+        _dialog.BottomConstraint = position.Bottom + (position.Height - dialogHeight) * 0.5f;
+        _dialog.MinWidthConstraint = dialogWidth;
+        _dialog.MaxWidthConstraint = dialogWidth;
+        _dialog.MaxHeightConstraint = dialogHeight;
+        _dialog.LayoutSelf();
+    }
+}
+
+public sealed class AddRepoDialog : View
+{
+    public AddRepoDialog(Action onClose)
+    {
+        AddChildToSelf(new RectView
+        {
+            BackgroundColor = 0xFF2A2D31,
+            BorderColor = BorderColorStyle.All(0xFF3D4147),
+            BorderSize = BorderSizeStyle.All(1),
+            Padding = PaddingStyle.All(16),
+            Children =
+            {
+                new FlexColumnView
+                {
+                    Gap = 12,
+                    MainAxisAlignment = MainAxisAlignment.SpaceEvenly,
+                    CrossAxisAlignment = CrossAxisAlignment.Stretch,
+                    Children =
+                    {
+                        new FlexRowView
+                        {
+                            MainAxisAlignment = MainAxisAlignment.SpaceBetween,
+                            CrossAxisAlignment = CrossAxisAlignment.Center,
+                            PreferredHeight = 24,
+                            Children =
+                            {
+                                new TextView
+                                {
+                                    Text = "Add Repository",
+                                    TextColor = 0xFFFFFFFF,
+                                    VerticalTextAlignment = TextAlignment.Center,
+                                },
+                                new DialogCloseButton(onClose),
+                            }
+                        },
+                        new DialogButton("Init New", () => { /* TODO */ })
+                        {
+                            PreferredHeight = 36,
+                        },
+                        new DialogButton("Clone", () => { /* TODO */ })
+                        {
+                            PreferredHeight = 36,
+                        },
+                        new DialogButton("Open", () => { /* TODO */ })
+                        {
+                            PreferredHeight = 36,
+                        },
+                    }
+                }
+            }
+        });
+    }
+}
+
+public sealed class DialogCloseButton : View
+{
+    public DialogCloseButton(Action onClick)
+    {
+        PreferredWidth = 24;
+        PreferredHeight = 24;
+        AddChildToSelf(new RectView
+        {
+            BackgroundColor = 0,
+            Children =
+            {
+                new TextView
+                {
+                    Text = "X",
+                    TextColor = 0xFFB5B9C0,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                }
+            }
+        });
+        Behaviors.Add(new DialogButtonController(onClick));
+    }
+}
+
+public sealed class DialogButton : View
+{
+    public DialogButton(string label, Action onClick)
+    {
+        AddChildToSelf(new RectView
+        {
+            BackgroundColor = 0xFF3D4147,
+            Children =
+            {
+                new TextView
+                {
+                    Text = label,
+                    TextColor = 0xFFFFFFFF,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                }
+            }
+        });
+        Behaviors.Add(new DialogButtonController(onClick));
+    }
+}
+
+public sealed class DialogButtonController(Action onClick) : KeyboardMouseController
+{
+    public override void OnMouseButtonStateChanged(ref MouseButtonEvent e)
+    {
+        if (e.Button == MouseButton.Left && e.State == InputState.Pressed)
+        {
+            onClick();
+            e.Consume();
+        }
     }
 }
 
---
+++ ZGF.Gui/Views/FlexColumnView.cs
@@ -0,0 +1,159 @@
+using ZGF.Geometry;
+
+namespace ZGF.Gui.Layouts;
+
+public sealed class FlexColumnView : View
+{
+    private float _gap;
+    public float Gap
+    {
+        get => _gap;
+        set => SetField(ref _gap, value);
+    }
+
+    private CrossAxisAlignment _crossAxisAlignment;
+    public CrossAxisAlignment CrossAxisAlignment
+    {
+        get => _crossAxisAlignment;
+        set => SetField(ref _crossAxisAlignment, value);
+    }
+
+    private MainAxisAlignment _mainAxisAlignment;
+    public MainAxisAlignment MainAxisAlignment
+    {
+        get => _mainAxisAlignment;
+        set => SetField(ref _mainAxisAlignment, value);
+    }
+
+    private readonly Dictionary<View, FlexStyle> _flexStyleByComponent = new();
+
+    public void UpdateStyle(View view, FlexStyle style)
+    {
+        _flexStyleByComponent[view] = style;
+        SetDirty();
+    }
+
+    protected override void OnComponentRemoved(View view)
+    {
+        _flexStyleByComponent.Remove(view);
+        base.OnComponentRemoved(view);
+    }
+
+    public override float MeasureHeight()
+    {
+        if (PreferredHeight.IsSet)
+            return PreferredHeight;
+
+        var totalHeight = 0f;
+        foreach (var child in Children)
+        {
+            totalHeight += child.MeasureHeight();
+        }
+        var spacing = (Children.Count - 1) * Gap;
+        return totalHeight + spacing;
+    }
+
+    protected override void OnLayoutChildren()
+    {
+        var position = Position;
+        var children = Children;
+        if (children.Count == 0)
+        {
+            return;
+        }
+
+        var totalChildrenInitialHeight = 0f;
+        var totalFlexGrow = 0f;
+
+        foreach (var child in children)
+        {
+            var style = _flexStyleByComponent.GetValueOrDefault(child);
+            totalChildrenInitialHeight += child.MeasureHeight();
+            totalFlexGrow += style.Grow;
+        }
+
+        var totalGap = Gap * (children.Count - 1);
+        var totalContentHeight = totalChildrenInitialHeight + totalGap;
+        var remainingSpace = position.Height - totalContentHeight;
+
+        var topOffset = 0f;
+        var interItemSpacing = 0f;
+
+        if (remainingSpace > 0)
+        {
+            switch (MainAxisAlignment)
+            {
+                case MainAxisAlignment.End:
+                    topOffset = remainingSpace;
+                    break;
+                case MainAxisAlignment.Center:
+                    topOffset = remainingSpace / 2f;
+                    break;
+                case MainAxisAlignment.SpaceBetween:
+                    interItemSpacing = children.Count > 1 ? remainingSpace / (children.Count - 1) : 0;
+                    break;
+                case MainAxisAlignment.SpaceAround:
+                    interItemSpacing = remainingSpace / children.Count;
+                    topOffset = interItemSpacing / 2f;
+                    break;
+                case MainAxisAlignment.SpaceEvenly:
+                    interItemSpacing = remainingSpace / (children.Count + 1);
+                    topOffset = interItemSpacing;
+                    break;
+            }
+        }
+
+        var currentTop = position.Top - topOffset;
+
+        foreach (var child in children)
+        {
+            var style = _flexStyleByComponent.GetValueOrDefault(child);
+            var childSize = child.MeasureSelf();
+            var childInitialWidth = childSize.Width;
+            var childInitialHeight = childSize.Height;
+
+            var finalChildHeight = childInitialHeight;
+            if (remainingSpace > 0 && style.Grow > 0 && totalFlexGrow > 0)
+            {
+                finalChildHeight += (style.Grow / totalFlexGrow) * remainingSpace;
+            }
+
+            float finalChildWidth;
+            float childLeft;
+
+            switch (CrossAxisAlignment)
+            {
+                case CrossAxisAlignment.Stretch:
+                    finalChildWidth = position.Width;
+                    childLeft = position.Left;
+                    break;
+
+                case CrossAxisAlignment.End:
+                    finalChildWidth = childInitialWidth;
+                    childLeft = position.Right - finalChildWidth;
+                    break;
+
+                case CrossAxisAlignment.Start:
+                    finalChildWidth = childInitialWidth;
+                    childLeft = position.Left;
+                    break;
+
+                case CrossAxisAlignment.Center:
+                default:
+                    finalChildWidth = childInitialWidth;
+                    var horizontalPadding = (position.Width - finalChildWidth) / 2f;
+                    childLeft = position.Left + horizontalPadding;
+                    break;
+            }
+
+            child.LeftConstraint = childLeft;
+            child.BottomConstraint = currentTop - finalChildHeight;
+            child.MinWidthConstraint = finalChildWidth;
+            child.MaxWidthConstraint = finalChildWidth;
+            child.MaxHeightConstraint = finalChildHeight;
+            child.LayoutSelf();
+
+            currentTop -= finalChildHeight + Gap + interItemSpacing;
+        }
+    }
+}
