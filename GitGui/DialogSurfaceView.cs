06e87d3a54bbc1b42cd2226cf848ba9cef1e14ab
Zee Vasilyev <zeejfps@gmail.com>
2026-05-19 14:47:19 -0500

    addded input blocking to the dialog surface view

--- GitGui/DialogSurfaceView.cs
+++ GitGui/DialogSurfaceView.cs
@@ -1,4 +1,5 @@
-﻿using ZGF.Gui;
+using ZGF.Gui;
+using ZGF.Gui.Tests;
 using ZGF.Gui.Views;
 
 namespace GitGui;
@@ -6,19 +7,24 @@ namespace GitGui;
 public sealed class DialogSurfaceView : MultiChildView
 {
     private readonly MultiChildView _overlay;
-    
+
     public DialogSurfaceView()
     {
-        _overlay = new RectView
+        _overlay = new MultiChildView
         {
-            BackgroundColor = 0xB0000000,
             ZIndex = 1000,
         };
     }
-    
+
     public void ShowDialog(View dialog)
     {
-        Children.Add(_overlay);
+        var backdrop = new RectView
+        {
+            BackgroundColor = 0xB0000000,
+        };
+        backdrop.UseController(_ => new DialogInputBlockingController());
+        _overlay.Children.Add(backdrop);
+
         _overlay.Children.Add(new CenterView
         {
             Children =
@@ -26,11 +32,31 @@ public sealed class DialogSurfaceView : MultiChildView
                 dialog,
             }
         });
+
+        Children.Add(_overlay);
     }
 
     public void HideDialog()
     {
         _overlay.Children.Clear();
         Children.Remove(_overlay);
+    }
+}
+
+internal sealed class DialogInputBlockingController : KeyboardMouseController
+{
+    public override void OnMouseButtonStateChanged(ref MouseButtonEvent e)
+    {
+        e.Consume();
+    }
+
+    public override void OnMouseWheelScrolled(ref MouseWheelScrolledEvent e)
+    {
+        e.Consume();
+    }
+
+    public override void OnMouseMoved(ref MouseMoveEvent e)
+    {
+        e.Consume();
     }
 }
