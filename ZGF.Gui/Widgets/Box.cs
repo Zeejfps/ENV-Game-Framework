99b6beba52b089c061f2f146b027b8a6bb2927ab
Zee Vasilyev <zeejfps@gmail.com>
2026-06-16 18:14:50 -0500

    Switched BorderRadius and BorderSize to Prop as well

--- ZGF.Gui/Widgets/Box.cs
+++ ZGF.Gui/Widgets/Box.cs
@@ -6,8 +6,8 @@ public sealed record Box : Widget
 {
     public Prop<uint> Background { get; init; }
     public Prop<PaddingStyle> Padding { get; init; }
-    public StyleValue<BorderRadiusStyle> BorderRadius { get; init; }
-    public StyleValue<BorderSizeStyle> BorderSize { get; init; }
+    public Prop<BorderRadiusStyle> BorderRadius { get; init; }
+    public Prop<BorderSizeStyle> BorderSize { get; init; }
     public Prop<BorderColorStyle> BorderColor { get; init; }
     public IWidget[] Children { get; init; } = [];
 
@@ -16,8 +16,8 @@ public sealed record Box : Widget
         var v = new RectView();
         Background.Apply(v, static (x, c) => x.BackgroundColor = c);
         Padding.Apply(v, static (x, p) => x.Padding = p);
-        if (BorderRadius.IsSet) v.BorderRadius = BorderRadius;
-        if (BorderSize.IsSet) v.BorderSize = BorderSize;
+        BorderRadius.Apply(v, static (x, r) => x.BorderRadius = r);
+        BorderSize.Apply(v, static (x, s) => x.BorderSize = s);
         BorderColor.Apply(v, static (x, c) => x.BorderColor = c);
         foreach (var child in Children)
             v.Children.Add(child.BuildView(ctx));
