dadea34af688a5eb878ae268f0f692fcc722b75c
Zee Vasilyev <zeejfps@gmail.com>
2026-06-21 22:28:00 -0500

    Made BoxShadowStyle a Prop

--- ZGF.Gui/Widgets/Box.cs
+++ ZGF.Gui/Widgets/Box.cs
@@ -8,6 +8,7 @@ public sealed record Box : Widget
     public Prop<BorderRadiusStyle> BorderRadius { get; init; }
     public Prop<BorderSizeStyle> BorderSize { get; init; }
     public Prop<BorderColorStyle> BorderColor { get; init; }
+    public Prop<BoxShadowStyle> Shadow { get; init; }
     public IWidget[] Children { get; init; } = [];
 
     protected override View CreateView(Context ctx)
@@ -17,6 +18,7 @@ public sealed record Box : Widget
         BorderRadius.Apply(ctx, v,static (x, r) => x.BorderRadius = r);
         BorderSize.Apply(ctx, v,static (x, s) => x.BorderSize = s);
         BorderColor.Apply(ctx, v,static (x, c) => x.BorderColor = c);
+        Shadow.Apply(ctx, v,static (x, s) => x.BoxShadow = s);
         foreach (var child in Children)
             v.Children.Add(child.BuildView(ctx));
         return v;
